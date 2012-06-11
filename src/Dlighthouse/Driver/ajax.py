0510c5da09cbdf4e6b3f09269a2f05ff6ca42f9b
Sa-Lin <devnull@localhost>
2012-06-11 09:33:32 -0500

    Generate Template button is working.

--- src/Dlighthouse/Driver/ajax.py
+++ src/Dlighthouse/Driver/ajax.py
@@ -4,26 +4,35 @@ from dajax.core import Dajax
 
 
 
-@dajaxice_register
-def selectedRoutines(request):
-	dajax = Dajax()	
-	#if request.method == 'POST':
-	routineCheckbox = request.POST.getlist('routineCheckbox')
-	#for item in routineCheckbox:
-		#dajax.script('dojo.create("li", {innerHTML: "%s"}, dojo.byId("selectedListNode"));' % item)
-	dajax.script('console.log("%s");' % routineCheckbox)
-	return dajax.json()
+#@dajaxice_register
+#def selectedRoutines(request):
+#	dajax = Dajax()	
+#	#if request.method == 'POST':
+#	routineCheckbox = request.POST.getlist('routineCheckbox')
+#	#for item in routineCheckbox:
+#		#dajax.script('dojo.create("li", {innerHTML: "%s"}, dojo.byId("selectedListNode"));' % item)
+#	dajax.script('console.log("%s");' % routineCheckbox)
+#	return dajax.json()
 
 
 
 @dajaxice_register
-def createTemplate(request, selectedRoutineNames):
+def createTemplate(request, selectedRoutine_ajax, selectedRoutine_session):
 	dajax = Dajax()
-	#delete the empty string in the selectedRoutineNames list
-	for item in filter(None, selectedRoutineNames):
-		innerHTMLText = "%s(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)" % item
-		dajax.script('dojo.create("div", {innerHTML: "%s"}, dojo.byId("div-codeTemp"));' % innerHTMLText)
-		
+	#dajax.script('console.log("1", selectedRoutineNames)')
+	#dajax.script('console.log("2", selectedRoutine_session)')
+	
+	# if there is a dnd action, use selectedRoutine_ajax; if not, use selectedRoutine_session.
+	if selectedRoutine_ajax:
+		#delete the empty string in the selectedRoutineNames list
+		for item in filter(None, selectedRoutine_ajax):
+			innerHTMLText = "%s(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)" % item
+			dajax.script('dojo.create("div", {innerHTML: "%s"}, dojo.byId("div-codeTemp"));' % innerHTMLText)
+	else:
+		for item in selectedRoutine_session:
+			innerHTMLText = "%s(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)" % item
+			dajax.script('dojo.create("div", {innerHTML: "%s"}, dojo.byId("div-codeTemp"));' % innerHTMLText)
+			
 	return dajax.json()
 		
 
