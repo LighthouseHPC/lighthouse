1df5c5801b09806f23a298cc97b24aa520718d7f
Sa-Lin <devnull@localhost>
2012-06-16 08:23:18 -0500

    modified routine ino files for the highlight in the simple search results.

--- src/Dlighthouse/RoutineInfo/routinePages.py
+++ src/Dlighthouse/RoutineInfo/routinePages.py
@@ -6,30 +6,36 @@ def file_name(precision, name, i):
 
 reader = csv.reader(open("url.csv"))
 
+delete = ("SUBROUTINE", "$", "--", "..", "=======")
+
 for idn, precision, routine, url in reader:
     URL = str("http://www.netlib.org/lapack/"+url)
     page = urllib.urlopen(URL)
     
     copy_page= open(file_name(precision, routine, idn), "w")
-
     
+    flag = 1
     while True:
-        content = page.readline()
+        content = page.readline()[1:]
 
         if "===============================================" in content:
             print idn, "--> find match!"
             break
 
-
-	if "Further Details" in content:
+        if "Further Details" in content:
             print idn, "--> find match!"
-            break		
-
+            break
 
         else:
-            copy_page.write(content)
+            if "SUBROUTINE" in content:
+                flag = 0
+            if "Purpose" in content:
+		flag = 1
+	    if flag and not "Purpose" in content:
+		if "=======" in content:
+		    content = ''
+		copy_page.write(content)
             
- 
     page.close()
     copy_page.close()
 
