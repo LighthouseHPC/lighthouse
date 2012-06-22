195063beb65d9c1f4b11fc465d588e5eb6e52880
Sa-Lin <devnull@localhost>
2012-06-22 17:01:04 -0500

    revised Driver/search_indexes.py.

--- src/Dlighthouse/Driver/search_indexes.py
+++ src/Dlighthouse/Driver/search_indexes.py
@@ -1,9 +1,9 @@
 import datetime
 from haystack.indexes import *
 from haystack import site
-from Driver.models import LinearEquation_simple, LinearEquation_expert, LinearEquation_driver
-from Driver.models import LinearLeastSquare
-from Driver.models import Eigensolver
+from Driver.models import LinearEquation_simple, LinearEquation_expert
+#from Driver.models import LinearLeastSquare
+#from Driver.models import Eigensolver
 #from Driver.models import SymmetricEigenvalue
 #from Driver.models import nonSymmetricEigenvalue
 
@@ -36,20 +36,7 @@ class LinearEquation_expertIndex(SearchIndex):
 
 
 
-class LinearEquation_driverIndex(SearchIndex):
-    text = CharField(document=True, use_template=True) 
-    thePrecision = CharField(model_attr='thePrecision')
-    routineName = CharField(model_attr='routineName')
-    matrixType = CharField(model_attr='matrixType')
-    storageType = CharField(model_attr='storageType')
-    url = CharField(model_attr='url')
-    notes = CharField(model_attr='notes')
-    info = CharField(model_attr='info')
-
- 
-
-
-
+'''
 class LinearLeastSquareIndex(SearchIndex):
     text = CharField(document=True, use_template=True) 
     thePrecision = CharField(model_attr='thePrecision')
@@ -61,7 +48,7 @@ class LinearLeastSquareIndex(SearchIndex):
     info = CharField(model_attr='info')
 
 
-"""
+
 class SymmetricEigenvalueIndex(SearchIndex):
     text = CharField(document=True, use_template=True) 
     thePrecision = CharField(model_attr='thePrecision')
@@ -83,7 +70,7 @@ class nonSymmetricEigenvalueIndex(SearchIndex):
     url = CharField(model_attr='url')
     notes = CharField(model_attr='notes')
     info = CharField(model_attr='info')
-"""
+
 
 
 class EigensolverIndex(SearchIndex):
@@ -95,15 +82,14 @@ class EigensolverIndex(SearchIndex):
     url = CharField(model_attr='url')
     notes = CharField(model_attr='notes')
     info = CharField(model_attr='info')
-
+'''
 
 site.register(LinearEquation_simple, LinearEquation_simpleIndex)
 site.register(LinearEquation_expert, LinearEquation_expertIndex)
-site.register(LinearEquation_driver, LinearEquation_driverIndex)
-site.register(LinearLeastSquare, LinearLeastSquareIndex)
+#site.register(LinearLeastSquare, LinearLeastSquareIndex)
 #site.register(SymmetricEigenvalue, SymmetricEigenvalueIndex)
 #site.register(nonSymmetricEigenvalue, nonSymmetricEigenvalueIndex)
-site.register(Eigensolver, EigensolverIndex)
+#site.register(Eigensolver, EigensolverIndex)
 
 
 
