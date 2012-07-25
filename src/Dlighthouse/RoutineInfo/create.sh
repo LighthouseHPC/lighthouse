50151adefb05e731b096a708fc5d1b0fa3748c53
Sa-Lin <devnull@localhost>
2012-07-25 12:45:47 -0500

    added RoutinrInfo/create.sh.

---
+++ src/Dlighthouse/RoutineInfo/create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash/
+#Program:
+#       Generate the routine info pages (.txt) in the RoutineTxt/ folder
+#Created on 07/25/2012 by Sa-Lin Cheng Bernstein
+
+
+PATH=/Applications/djangostack-1.3.1-4/mysql/bin:/Applications/djangostack-1.3.1-4/python/bin:/Applications/djangostack-1.3.1-4/mysql/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/usr/texbin:/usr/X11/bin
+
+export PATH
+
+echo "Running 'routinePages.py' to generate the .txt files..."
+python routinePages.py
+
+echo ".txt files are created!"
+
+echo "Running 'routineList.py' to create routine_info.csv for the database..."
+
+python routineList.py
+
+echo "routine_info.csv is created!"
