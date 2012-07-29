a7103d2ebea85ef54adf5eef7d8119f7159e2476
Sa-Lin <devnull@localhost>
2012-07-29 09:54:51 -0500

    fixed the duplicates in lapack341/sort341/all.py.

--- src/LAPACK341/sort341/all.py
+++ src/LAPACK341/sort341/all.py
@@ -6,7 +6,7 @@ os.sys.path.insert(0,parentdir)
 import summary.summary
 
 
-print "----------------- Sort the sing routines in v3.4.1 -----------------"
+print "----------------- Fild all the routines in v3.4.1 -----------------"
 
 
 ###----------- get new_list
@@ -27,18 +27,31 @@ for routineName in new_list:
     text = text.split("\n")
     for line in text:
         line = line[3:]
-        if line.startswith("\ingroup"):
-            i += 1
-            category = line.split(" ")[1]
-            routines_all.append(routineName)
-            print i, " ", routineName, " ", category
-            wr_all.writerow([i, routineName[0], routineName[1:-2], "http://www.netlib.org/lapack/lapack_routine/"+routineName])
-            wr_list.writerow([i, routineName, category])
+        if "=============================================================" in line:
+            break
         else:
-            pass
+            if line.startswith("\ingroup"):
+                i += 1
+                category = line.split(" ")[1]
+                routines_all.append(routineName)
+                print i, " ", routineName, " ", category
+                wr_all.writerow([i, routineName[0], routineName[1:-2], "http://www.netlib.org/lapack/lapack_routine/"+routineName])
+                wr_list.writerow([i, routineName, category])
+            else:
+                pass
 
     f.close()
+    
+print "================================================"  
 
+dups = [x for x in routines_all if routines_all.count(x) > 1]
+print "Duplicate elements:"
+for item in dups:
+    print item
+print "================================================"
+
+
+routines_all = [x for x in routines_all if not routines_all.count(x) > 1]
 
 print "Number of routine in v3.4.1 : ", len(routines_all)
 
