fceddda36290a10f075b15c54fd5a7c41a85d935
Sa-Lin <devnull@localhost>
2012-07-30 18:29:23 -0500

    modified lapack341/errorBounds/.

--- src/LAPACK341/errorBounds/errorBound_find_old.py
+++ src/LAPACK341/errorBounds/errorBound_find_old.py
@@ -3,41 +3,57 @@ from time import time
 import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir) 
-import summary
+import summary.summary
 
+
+def findRoutines(fileName):
+    for ln in fileName:
+        url = ln.split(";")[3]
+        routineName = url.split("/")[-1]
+        #print routineName
+        #print url
+        f = urllib.urlopen(url)
+        flag = 1
+        for line in f:
+            line = line[3:]
+            #print line
+            if line.startswith("Arguments"):
+                break
+            else:
+                if line.startswith("\par Purpose:"):
+                    flag = 0
+                if line.startswith("Arguments"):
+                    flag = 1
+                if not flag:
+                    index1 = line.find("error bounds")
+                    if index1 > -1:
+                        routines_errorBound_old.append(routineName)
+                        f_errorBound_old.write(routineName)
+                    else:
+                        pass
+
+    fileName.close()
 
 print "------------- Find 'error bounds' routines in the old version --------------"
-
-old_list_url = summary.Old_List_URL()
 
 
 ###------------find 'error bounds" routines in the old version
 ###------------ and write them into routines/errorBound_old.txt
 ## find the routines that HAVE the keywords:
-f_errorBound_old = open('routines/errorBound_old.txt', 'w')
+f_computational_old_single = open(parentdir+'/sortOld/routines/computational_old_single.txt')
+f_computational_old_double = open(parentdir+'/sortOld/routines/computational_old_double.txt')
+f_computational_old_complex = open(parentdir+'/sortOld/routines/computational_old_complex.txt')
+f_computational_old_complex16 = open(parentdir+'/sortOld/routines/computational_old_complex16.txt')
+f_errorBound_old = open('./routines/errorBound_old.txt', 'w')
 routines_errorBound_old = []
 start = time()
-for url in old_list_url:
-    f_info = urllib.urlopen(url)
-    routineName = url.split("/")[-1]
-    flag = 1
-    for line in f_info:
-        if "================================" in line:
-            break
-        else:
-            line = line[3:]
-            if line.startswith("Purpose"):
-                flag = 0
-            if line.startswith("Arguments"):
-                flag = 1
-            if not flag:
-                index1 = line.find("error bounds")
-                if index1 > -1:
-                    routines_errorBound_old.append(routineName)
-                    f_errorBound_old.write(routineName+'\n')
-                else:
-                    pass
-    f_info.close()
+
+
+findRoutines(f_computational_old_single)
+findRoutines(f_computational_old_double)
+findRoutines(f_computational_old_complex)
+findRoutines(f_computational_old_complex16)
+
     
 elapsed = (time() - start)
 print "There are %s routines in the old version that provides error bounds." % len(routines_errorBound_old), elapsed
