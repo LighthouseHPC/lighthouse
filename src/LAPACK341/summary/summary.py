24d26e8661d30fdb83e1c2e169191deeb63ce068
Sa-Lin <devnull@localhost>
2012-07-30 12:19:17 -0500

    changed the urls' in Old_List_URL() in lapack341/summary/summary.py.

--- src/LAPACK341/summary/summary.py
+++ src/LAPACK341/summary/summary.py
@@ -74,25 +74,25 @@ def Old_List_URL():
 
     for line in f_single:
         if "href=" in line:
-            old_list_url.append("http://www.netlib.org/lapack/single/"+line.split('\"')[1])
+            old_list_url.append("http://www.netlib.org/lapack/lapack_routine/"+line.split('\"')[1])
         else:
             pass
        
     for line in f_double:
         if "href=" in line:
-            old_list_url.append("http://www.netlib.org/lapack/double/"+line.split('\"')[1])
+            old_list_url.append("http://www.netlib.org/lapack/lapack_routine/"+line.split('\"')[1])
         else:
             pass
     
     for line in f_complex:
         if "href=" in line:
-            old_list_url.append("http://www.netlib.org/lapack/complex/"+line.split('\"')[1])
+            old_list_url.append("http://www.netlib.org/lapack/lapack_routine/"+line.split('\"')[1])
         else:
             pass
     
     for line in f_complex16:
         if "href=" in line:
-            old_list_url.append("http://www.netlib.org/lapack/complex16/"+line.split('\"')[1])
+            old_list_url.append("http://www.netlib.org/lapack/lapack_routine/"+line.split('\"')[1])
         else:
             pass
     
