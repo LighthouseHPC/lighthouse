acb981933ae3f9fc848487b9c20b9108e5171660
Sa-Lin <devnull@localhost>
2012-08-09 14:22:07 -0500

    addede lapack341/pivotGrowthFactor/ and lapack341/refinement/.

---
+++ src/LAPACK341/pivotGrowthFactor/pivotGrowthFactor.py
@@ -0,0 +1,68 @@
+import urllib, shutil, csv
+from time import time
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir) 
+import summary.summary
+
+def findRoutines(fileName):
+    for ln in fileName:
+        url = ln.split(";")[3]
+        routineName = url.split("/")[-1]
+        #print routineName
+        #print url
+        f = urllib.urlopen(url)
+        flag = 1
+        for line in f:
+            line = line[3:]
+            #print line
+            if line.startswith("Arguments"):
+                break
+            else:
+                if line.startswith("\par Purpose:"):
+                    flag = 0
+                if line.startswith("Arguments"):
+                    flag = 1
+                if not flag:
+                    index1 = line.find("pivot growth factor")
+                    if index1 > -1:
+                        routines_pivot_growth_factor_341.append(routineName)
+                        f_pivot_growth_factor_341.write(routineName)
+                    else:
+                        pass
+
+    fileName.close()
+
+print "------------- Find 'pivot growth factor' routines in v3.4.1 --------------"
+
+
+###------------ find routines that compute the pivot growth factor of a matrix in the new version
+###------------ and write them into routines/pivot_growth_factor_341.txt
+## find the routines that HAVE the keywords:
+f_solve_341_single = open(parentdir+'/sort341/routines/solve_341_single.txt')
+f_solve_341_double = open(parentdir+'/sort341/routines/solve_341_double.txt')
+f_solve_341_complex = open(parentdir+'/sort341/routines/solve_341_complex.txt')
+f_solve_341_complex16 = open(parentdir+'/sort341/routines/solve_341_complex16.txt')
+
+f_computational_341_single = open(parentdir+'/sort341/routines/computational_341_single.txt')
+f_computational_341_double = open(parentdir+'/sort341/routines/computational_341_double.txt')
+f_computational_341_complex = open(parentdir+'/sort341/routines/computational_341_complex.txt')
+f_computational_341_complex16 = open(parentdir+'/sort341/routines/computational_341_complex16.txt')
+
+f_pivot_growth_factor_341 = open('./routines/pivot_growth_factor_341.txt', 'w')
+routines_pivot_growth_factor_341 = []
+start = time()
+
+
+findRoutines(f_solve_341_single)
+findRoutines(f_solve_341_double)
+findRoutines(f_solve_341_complex)
+findRoutines(f_solve_341_complex16)
+findRoutines(f_computational_341_single)
+findRoutines(f_computational_341_double)
+findRoutines(f_computational_341_complex)
+findRoutines(f_computational_341_complex16)
+    
+elapsed = (time() - start)
+print "There are %s routines in the 341 version that provides pivot growth factor." % len(routines_pivot_growth_factor_341), elapsed
+
---
+++ src/LAPACK341/refinement/refinement.py
@@ -0,0 +1,101 @@
+import urllib, shutil, csv
+from time import time
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir) 
+import summary.summary
+
+def findRoutines(fileName):
+    for ln in fileName:
+        url = ln.split(";")[3]
+        routineName = url.split("/")[-1]
+        #print routineName
+        #print url
+        f = urllib.urlopen(url)
+        flag = 1
+        for line in f:
+            line = line[3:]
+            #print line
+            if line.startswith("Arguments"):
+                break
+            else:
+                if line.startswith("\par Purpose:"):
+                    flag = 0
+                if line.startswith("Arguments"):
+                    flag = 1
+                if not flag:
+                    index1 = line.find("refinement")
+                    if index1 > -1:
+                        routines_refinement_341.append(routineName)
+                    else:
+                        pass
+
+
+    fileName.close()
+
+print "------------- Find 'refinement' routines in v3.4.1 --------------"
+
+
+###------------ find routines that compute the refinement of a matrix in the new version
+###------------ and write them into routines/refinement_341.txt
+## find the routines that HAVE the keywords:
+f_solve_341_single = open(parentdir+'/sort341/routines/solve_341_single.txt')
+f_solve_341_double = open(parentdir+'/sort341/routines/solve_341_double.txt')
+f_solve_341_complex = open(parentdir+'/sort341/routines/solve_341_complex.txt')
+f_solve_341_complex16 = open(parentdir+'/sort341/routines/solve_341_complex16.txt')
+
+f_computational_341_single = open(parentdir+'/sort341/routines/computational_341_single.txt')
+f_computational_341_double = open(parentdir+'/sort341/routines/computational_341_double.txt')
+f_computational_341_complex = open(parentdir+'/sort341/routines/computational_341_complex.txt')
+f_computational_341_complex16 = open(parentdir+'/sort341/routines/computational_341_complex16.txt')
+
+f_refinement_341 = open('./routines/refinement_341.txt', 'w')
+f_not_refinement_341 = open('./routines/not_refinement_341.txt', 'w')
+routines_refinement_341 = []
+routines_not_refinement_341 = []
+start = time()
+
+
+findRoutines(f_solve_341_single)
+findRoutines(f_solve_341_double)
+findRoutines(f_solve_341_complex)
+findRoutines(f_solve_341_complex16)
+findRoutines(f_computational_341_single)
+findRoutines(f_computational_341_double)
+findRoutines(f_computational_341_complex)
+findRoutines(f_computational_341_complex16)
+
+
+## find the routines that do not do iterative refinement
+for routineName in routines_refinement_341:
+    f = urllib.urlopen('http://www.netlib.org/lapack/lapack_routine/'+routineName)
+    flag = 1
+    for line in f:
+        line = line[3:]
+        #print line
+        if line.startswith("Arguments"):
+            break
+        else:
+            if line.startswith("\par Purpose:"):
+                flag = 0
+            if line.startswith("Arguments"):
+                flag = 1
+            if not flag:        
+                index2 = line.find("does not do iterative")
+                if index2 > -1:
+                    routines_not_refinement_341.append(routineName)
+                    f_not_refinement_341.write(routineName)
+                else:
+                    pass
+                    
+
+## substarct the routines that do not do iterative refinement.
+routines_refinement_341 = list(set(routines_refinement_341) - set(routines_not_refinement_341))
+
+for routineName in routines_refinement_341:
+    f_refinement_341.write(routineName)
+
+elapsed = (time() - start)
+print "%s routines that do not do iterative refinement." % len(routines_not_refinement_341)
+print "There are %s routines in the 341 version that provides refinement." % len(routines_refinement_341), elapsed
+
