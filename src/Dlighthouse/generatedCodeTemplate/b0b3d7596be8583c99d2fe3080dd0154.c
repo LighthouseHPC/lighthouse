45eb9753d2bf923b8c4523eaed8e8dce48f823e1
javed <devnull@localhost>
2012-08-10 09:30:45 -0600

    Added move.png

---
+++ src/Dlighthouse/generatedCodeTemplate/b0b3d7596be8583c99d2fe3080dd0154.c
@@ -0,0 +1,4 @@
+cspsvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+dptsvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+sptsvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+chpsvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
---
+++ src/Dlighthouse/generatedCodeTemplate/bdbe5ceba6c4bc4bbdd3f83b332d4ed1.c
@@ -0,0 +1,2 @@
+sgesv(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+dgesv(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
