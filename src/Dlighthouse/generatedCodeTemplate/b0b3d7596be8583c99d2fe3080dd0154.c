6af6a73272522453cb4fe2a5ec71bd83ce6ff342
javed <devnull@localhost>
2012-08-14 19:53:46 -0600

    Routines in the Selected Routine area can now be selected by clicking on the routine name.

--- src/Dlighthouse/generatedCodeTemplate/b0b3d7596be8583c99d2fe3080dd0154.c
+++ src/Dlighthouse/generatedCodeTemplate/b0b3d7596be8583c99d2fe3080dd0154.c
@@ -1,3 +1,5 @@
-cgesvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
-sgesvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+cpbsvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
 dgesvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+cgesvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+cgbsvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
+sgesvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
