9adc0833843981453739d289c1f247deccfe87b7
Sa-Lin <devnull@localhost>
2012-08-20 16:13:41 -0500

    modified Driver/dataBase/le_driver_all.py.

--- src/Dlighthouse/Driver/dataBase/le_driver_all.py
+++ src/Dlighthouse/Driver/dataBase/le_driver_all.py
@@ -7,14 +7,13 @@ f_le_simple = csv.reader(open('le_simple.csv', 'rb'), delimiter=',')
 f_le_expert = csv.reader(open('le_expert.csv', 'rb'), delimiter=',')
 f_le_driver_all = csv.writer(open('le_driver_all.csv', 'wb'), delimiter=',')
 
-i = 1
-for row in f_le_simple:
-    i = row[0]
-    f_le_driver_all.writerow(row)
-    
+
+def writeRows(fileName, lineNumber):
+    for row in fileName:
+        row[0] = int(row[0]) + lineNumber
+        f_le_driver_all.writerow(row)
 
 
-for row in f_le_expert:
-    row[0] = int(row[0])+ int(i)
-    row[0] = str(row[0])
-    f_le_driver_all.writerow(row)    
+writeRows(f_le_simple, 0)
+
+writeRows(f_le_expert, f_le_simple.line_num)
