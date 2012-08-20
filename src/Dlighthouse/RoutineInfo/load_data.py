0d83733d5ab0ea3872ba39f66ed584e27083e024
Sa-Lin <devnull@localhost>
2012-08-20 15:59:19 -0500

    modified RoutineInfo/load_data.py and RoutineInfo/docGene/url.csv.

--- src/Dlighthouse/RoutineInfo/load_data.py
+++ src/Dlighthouse/RoutineInfo/load_data.py
@@ -6,11 +6,11 @@ myDB = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="yellow1
 cursor = myDB.cursor()
 
 
-reader = csv.reader(open("/Users/salin/Documents/Lighthouse/Dlighthouse/RoutineInfo/routine_info.csv"))
+reader = csv.reader(open("docGene/routine_info.csv"))
 
 i = 1
 for file in reader:
-	fd = open("/Users/salin/Documents/Lighthouse/Dlighthouse/RoutineInfo/RoutineTxt/"+file[0], "r")
+	fd = open("docGene/RoutineTxt/"+file[0], "r")
 	myDB.query("insert into Driver_routineinfo (id, routine, info) values (\'%d\', \'%s\', \'%s\')" % (i, file[0], myDB.escape_string(fd.read())))
 	i += 1
 
