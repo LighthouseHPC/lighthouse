d075b5e61238f060bc494b92d204129b1b9f9720
Sa-Lin <devnull@localhost>
2012-08-28 10:27:33 -0500

    added computational_solve/routines/solve_341_sort.txt.d

--- src/LAPACK341/computational_solve/database_check.py
+++ src/LAPACK341/computational_solve/database_check.py
@@ -13,12 +13,25 @@ for line in f_database:
     
 f_solve_341 = open('./routines/solve_341.txt')
 solve341Routines = []
+solve341Routines_trs = []
+keys = []
 for line in f_solve_341:
-    solve341Routines.append(line.rstrip('.f\r\n'))
-    
+    line = line.rstrip('\r\n')
+    routine = line.split('.')[0]
+    solve341Routines.append(routine)
+    last3 = routine[-3:]
+    keys.append(last3)
+    if last3 == 'trs':
+        solve341Routines_trs.append(routine)
+    else:
+        pass
 
-missingRoutines = list(set(solve341Routines) - set(databaseRoutines))
 
+solve341Routines = set(solve341Routines)
+
+#print solve341Routines
+
+missingRoutines = list(set(solve341Routines_trs) - set(databaseRoutines))
 
 f_missing = open('./routines/missingRoutines.txt', 'w')
 
@@ -28,5 +41,22 @@ for routine in missingRoutines:
 
 print "%s computational_solve routines may need to be updated in the database." % len(missingRoutines)
 
+
+### sort the routines in ./routines/solve_341.txt
+f_solve_341_sort = open('./routines/solve_341_sort.txt', 'w')
+keys = set(keys)
+print keys
+
+for element in keys:
+    i = 0
+    for routine in solve341Routines:
+        if element == routine[-3:]:
+            i += 1
+            f_solve_341_sort.write(routine+'\n')
+        
+    f_solve_341_sort.write('-------------------------%s\r\n\n' % i)
+
+
+f_solve_341_sort.close()
 f_solve_341.close()
 f_missing.close()
