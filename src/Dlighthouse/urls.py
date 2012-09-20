520a3fafa48ebac98cc12015630ad893e28735f5
Sa-Lin <devnull@localhost>
2012-09-20 15:10:56 -0500

    cleaned up the main urls.py.

--- src/Dlighthouse/urls.py
+++ src/Dlighthouse/urls.py
@@ -2,46 +2,54 @@ from django.conf.urls.defaults import *
 from Dlighthouse import settings
 #from Dlighthouse.views import *
 
-# Uncomment the next two lines to enable the admin:
+### Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-# To enable dajaxice
+### To enable dajaxice
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 dajaxice_autodiscover()
 
 
 urlpatterns = patterns('',
-    # Example:
+    ### Example:
     # (r'^Dlighthouse/', include('Dlighthouse.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
+    ### Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    ### to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    # Go to the login page
-    (r'^$', 'django.contrib.auth.views.login'),
-    (r'^index/$', 'django.contrib.auth.views.login'),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    ###  dajaxice URLS
+    #url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 
-    #  dajaxice URLS
-    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
-
-    # For dojango
+    ### For dojango
     #(r'^dojango/', include('dojango.urls')),
 
-    # For Haystack
+    ### For Haystack
     #(r'^search/', include('haystack.urls')),
+    
+    
 
+    ### Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+
+    ### Use the files in the media/ directory:
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    
+    ### Use the files in the templates/ dirctory
     (r'^templates/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.TEMPLATE_ROOT}),
     
-    # Link Driver/urls.py for guided, advanced, and keyword Searches.
+    ### Go to the login page
+    (r'^$', 'django.contrib.auth.views.login'),
+    (r'^index/$', 'django.contrib.auth.views.login'),
+    
+    ### Link Driver/urls.py for guided, advanced, and keyword Searches:
     (r'^search/', include('Driver.urls')),
     
-    # Link registration/backends/default/urls/py for account registration and login/logout.
-    (r'^accounts/', include('emailRegistration.urls')),
+    ### Link registration/backends/default/urls.py for normal account registration:
+    #(r'^accounts/', include('registration.backends.default.urls')),
+    
+    ### Link emailRegistartion/urls.py to use email as username:
+    (r'^accounts/', include('emailRegistration.urls'))
 
 )
