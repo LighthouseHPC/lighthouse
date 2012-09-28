e2bebbd08d24306611cf6ae7a7e7166b05d7aa9f
Sa-Lin <devnull@localhost>
2012-09-28 15:03:41 -0500

    fixed the KeyError: 'email' problem caused by submiting an empty registration form.

--- src/Dlighthouse/emailRegistration/__init__.py
+++ src/Dlighthouse/emailRegistration/__init__.py
@@ -9,6 +9,8 @@ from registration.forms import RegistrationForm
 from registration.models import RegistrationProfile
 from registration.backends.default import DefaultBackend
 
+from django.http import HttpResponseBadRequest
+
 class EmailRegistrationForm(RegistrationForm):
     def __init__(self, *args, **kwargs):
         super(EmailRegistrationForm,self).__init__(*args, **kwargs)
@@ -18,6 +20,11 @@ class EmailRegistrationForm(RegistrationForm):
         cleaned_data = super(EmailRegistrationForm,self).clean()
         if 'email' in self.cleaned_data:
             cleaned_data['username'] = self.cleaned_data['username'] = self.cleaned_data['email']
+        else:
+            """ When submit an empty form, show required fields without raising a new ValidationError.
+            """
+            return HttpResponseBadRequest()
+
         if User.objects.filter(email__iexact=self.cleaned_data['email']):
             raise forms.ValidationError("This email address is already in use. Please supply a different email address.")
         return cleaned_data
