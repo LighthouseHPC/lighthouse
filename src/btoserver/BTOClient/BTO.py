be5ba36ed20c1e3e9e0a36abcfd8491d17e393ed
Sa-Lin <devnull@localhost>
2012-10-20 08:20:51 -0500

    revised BTO.py again.

--- src/btoserver/BTOClient/BTO.py
+++ src/btoserver/BTOClient/BTO.py
@@ -1,7 +1,8 @@
+import os, os.path
+import glob, shutil
+from subprocess import call
 from Base import BaseServer,BaseClient
 from utils import change_workdir, remove_workdir
-from subprocess import call
-import os,os.path, glob
 
 ########################################################################
 class BTO_Server(BaseServer):
@@ -27,9 +28,9 @@ class BTORequestHandler(BaseServer):
         
     def bto_handle(self):
         os.chdir('/tmp')
-        ###---- created /tmp/userid+"_"+self.req_id and set it to be workdir
+        ###---- create userid+"_"+self.req_id and name it baseworkdir
         baseworkdir = self.users[0]+'_'+ self.req_id
-        #print baseworkdir                  #salin_17-47-53
+        #print baseworkdir                  #salin_xx-xx-xx
 	os.mkdir(baseworkdir)
         try:
             userid  = self.check_user(self.recv_header1())
@@ -37,45 +38,46 @@ class BTORequestHandler(BaseServer):
 	    nfiles  = int(self.recv_header1())
         except:
             with open(baseworkdir+'/errors.x', 'w') as f:
-                f.write('An error occurred while the BTO server was receiving the input file: invalid userid or options.')
+                f.write('An error occurred while the BTO server was receiving the input file.')
             self.send_header1(1)
             self.send_files([baseworkdir+'/errors.x'])
             return None
 	else:
+            ###---- set /tmp/userid+"_"+self.req_id to be workdir
             os.chdir(baseworkdir)
             workdir = os.getcwd()
-            #print workdir			#/tmp/salin_17-47-53
+            #print workdir			#/tmp/salin_xx-xx-xx
             files = self.recv_files(nfiles)
             filename = files[0]
 
-	    ###---- change dir to bto/ in order to run ./bin/btiblas
+	    ###---- change dir to bto/ in order to execute ./bin/btiblas
             os.chdir(self.bto_dir)
             #print "Current folder is:", os.getcwd()			#/homes/salin/Lighthouse/BTOServer/bto
-	    #print ".m file location:", workdir + '/' +filename 	#/tmp/salin_17-59-44/DGEM.m
+	    #print ".m file location:", workdir + '/' +filename 	#/tmp/salin_xx-xx-xx/DGEM.m
             try:
             	call([self.bto_blas, workdir + '/' +filename])
             except:
-            	self.send_error('Failed to execute ./bin/btoblas %s'%workdir+ '/' +filename)
+            	self.send_error("Failed to execute ./bin/btoblas %s" %workdir+ "/" +filename)
 	    else:
+		###---- go back to /tmp/salin_xx-xx-xx to check if the c files are generated
             	os.chdir(workdir)
             	cfiles = glob.glob('*.c')
             	if(len(cfiles) == 1): 
                 	self.send_header1(1)
                 	self.send_files(cfiles)
             	else:
-                	with open('errors.x', 'w') as f:
+                	with open(baseworkdir+'errors.x', 'w') as f:
                     		f.write('The BTO server was unable to compile and generate an output file')
                 	self.send_header1(1)
-                	self.send_files(['errors.x'])
+                	self.send_files([baseworkdir+'errors.x'])
 
 	finally:
-	    ###---- delete /tmp/userid+"_"+self.req_id/ folder
-            import shutil
+	    ###---- delete the /tmp/userid+"_"+self.req_id/ folder
             shutil.rmtree(workdir)
 	    if os.path.exists(workdir) == False:
-		print "%s is removed successfully!"%workdir
+		print "%s is removed successfully!" %workdir
 	    else:
-		print "%s is not yet removed."%workdir
+		print "%s is not yet removed." %workdir
 
 
     
