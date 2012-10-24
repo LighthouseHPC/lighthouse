d41fc9c92658fc3fbace48ca56a6c6b4db5aec7f
sasa1688 <sasa1688@gmail.com>
2012-10-24 17:42:38 +0000

    corrected USER in BTOClient/bto_client.py.

--- src/btoserver/bto_client.py
+++ src/btoserver/bto_client.py
@@ -2,9 +2,9 @@ from BTO import BTO_Client
 
 HOST = 'localhost'
 PORT = 9999
-USER = 'dljohnso'
+USER = 'salin'
 OPTIONS = '-e'
-FILE = 'dgemv_row.m'
+FILE = 'dgemv.m'
 
 client = BTO_Client()
 client.submit_request(HOST, PORT, USER, OPTIONS, FILE)
