4d54e8eac4c9b8c0eb063e31481e4a58e9f5609e
Sa-Lin <devnull@localhost>
2013-01-24 14:04:02 -0600

    added lapack tag to blog.

--- src/Dlighthouse/Driver/urls.py
+++ src/Dlighthouse/Driver/urls.py
@@ -1,29 +1,27 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-from Driver.views import *
 
-
-urlpatterns = patterns('',
+urlpatterns = patterns('Driver.views',
 ### --- The following lines use Driver.views --- ###
-    (r'^$', search_forms),
-    (r'^guided/problem/$', guidedSearch_problem), 
-    (r'^guided/problem_equation/$', guidedSearch_equation),
-    (r'^guided/problem_equation_factor/$', guidedSearch_factor),
-    (r'^guided/problem_equation_factor_complex$', guidedSearch_complex),
-    (r'^guided/problem_complex/$', guidedSearch_complex),
-    (r'^guided/problem_complex_matrixtype/$', guidedSearch_matrixtype),   
-    (r'^guided/problem_complex_matrixtype_storage/$', guidedSearch_storage),
-    (r'^guided/problem_complex_matrixtype_storage_precision/$', guidedSearch_precision),
+    url(r'^$', "search_forms", name="lapack"),
+    url(r'^guided/problem/$', "guidedSearch_problem"), 
+    url(r'^guided/problem_equation/$', "guidedSearch_equation"),
+    url(r'^guided/problem_equation_factor/$', "guidedSearch_factor"),
+    url(r'^guided/problem_equation_factor_complex$', "guidedSearch_complex"),
+    url(r'^guided/problem_complex/$', "guidedSearch_complex"),
+    url(r'^guided/problem_complex_matrixtype/$', "guidedSearch_matrixtype"),   
+    url(r'^guided/problem_complex_matrixtype_storage/$', "guidedSearch_storage"),
+    url(r'^guided/problem_complex_matrixtype_storage_precision/$', "guidedSearch_precision"),
 
-    (r'^advanced/form/$', advancedForm),
-    (r'^advanced/result/$', advancedResult),
+    url(r'^advanced/form/$', "advancedForm"),
+    url(r'^advanced/result/$', "advancedResult"),
     
-    (r'^update_session/$', update_session),
-    (r'^clear_session/$', clear_session),    
-    (r'^load_template/$', load_template),
+    url(r'^update_session/$', "update_session"),
+    url(r'^clear_session/$', "clear_session"),    
+    url(r'^load_template/$', "load_template"),
     
-    (r'^keyword/$', keywordResult),
-    (r'^download/$', downloadTemplate),
+    url(r'^keyword/$', "keywordResult"),
+    url(r'^download/$', "downloadTemplate"),
     #(r'^script/$', runScript),
 )
