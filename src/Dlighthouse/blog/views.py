7fc5827611a603c4fbe4988f0db96162f48eef55
Sa-Lin <devnull@localhost>
2013-02-01 15:35:07 -0600

    blog haystack search working.

--- src/Dlighthouse/blog/views.py
+++ src/Dlighthouse/blog/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render_to_response, redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 from django import forms
+from django.forms import ModelForm
 
 from haystack.views import SearchView 
 from haystack.query import SearchQuerySet
@@ -15,30 +17,9 @@ from haystack.forms import ModelSearchForm
 from calendar import month_name
 import time
 
-from blog.models import *
-from blog.forms import CustomModelSearchForm
+from blog.models import Post, Comment, CommentForm
 
 
-
-""" modify Haystack ModelSearchForm --> CUSTOM_CHOICES """
-CUSTOM_CHOICES = (
-    ('blog.Post', 'Posts'),
-    ('blog.Comment', 'Comments'),
-)
-
-
-class CustomModelSearchForm(ModelSearchForm):
-    def __init__(self, *args, **kwargs):
-        super(CustomModelSearchForm, self).__init__(*args, **kwargs)
-        self.fields['models'] = forms.MultipleChoiceField(
-            label='Search In',
-            choices=CUSTOM_CHOICES,
-            required=False,
-            widget=forms.CheckboxSelectMultiple
-            )
-        
-        
-        
 
 
 
@@ -51,19 +32,26 @@ class MyHaystackSearchView(SearchView):
     def extra_context(self):
         return {
             "months": mkmonth_lst(),
-            "searchForm": CustomModelSearchForm()
+            "searchForm": ModelSearchForm()
         }
 
 
 
 
 def MySearchView(request):
+    modelList = []
     if request.method == 'GET': # If the form has been submitted...
-        form = CustomModelSearchForm(request.GET) # A form bound to the GET data
-        form.search
+        form = ModelSearchForm(request.GET) # A form bound to the GET data
         if form.is_valid(): # All validation rules pass
-            ### only search in the models 'Post' and 'Comment'.
-            sqs = SearchQuerySet().models(Post, Comment)
+            answer = form.cleaned_data['models']
+            if answer == []:
+                sqs = SearchQuerySet().models(Post, Comment)
+            else:
+                for model_name in answer:
+                    # Turen a string into a class and create a list of model classes for sqs
+                    ct = ContentType.objects.get(model=model_name.split(".")[1])
+                    modelList.append(ct.model_class())
+                sqs = SearchQuerySet().models(*modelList)
             search_view = MyHaystackSearchView(template = "blog/blog_search.html", searchqueryset=sqs)
             return search_view(request, "hello")
 
@@ -90,7 +78,7 @@ def main(request):
     except (InvalidPage, EmptyPage):
         posts = paginator.page(paginator.num_pages)
 
-    d = {"posts": posts, "user": request.user, "months": mkmonth_lst(), "searchForm": CustomModelSearchForm()}
+    d = {"posts": posts, "user": request.user, "months": mkmonth_lst(), "searchForm": ModelSearchForm()}
     return render_to_response("blog/list.html", d)
 
 
@@ -100,7 +88,7 @@ def post(request, pk):
     """post below gets the title of the post. note: Post.objects.get()"""
     post = Post.objects.get(pk=int(pk))
     comments = Comment.objects.filter(title=post)
-    d = {"post":post, "user":request.user, "months": mkmonth_lst(), "comments": comments, "form": CommentForm(), "searchForm": SearchForm()}
+    d = {"post":post, "user":request.user, "months": mkmonth_lst(), "comments": comments, "form": CommentForm(), "searchForm": ModelSearchForm()}
     d.update(csrf(request))
     return render_to_response("blog/post.html", d)
 
