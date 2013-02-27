4a83d0b409a36355acbd7de3d82bdf573f85686c
Sa-Lin <devnull@localhost>
2013-02-26 18:27:58 -0600

    updated truncateTables.py.

--- src/Dlighthouse/lighthouse/libraries/lapack_le/databaseMng/truncateTables.py
+++ src/Dlighthouse/lighthouse/libraries/lapack_le/databaseMng/truncateTables.py
@@ -5,24 +5,24 @@ conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="yellow1
 cursor = conn.cursor()
 
 
-cursor.execute("TRUNCATE TABLE lapack_linearequation_driver")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_expert")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_simple")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_driver")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_expert")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_simple")
 
 
-cursor.execute("TRUNCATE TABLE lapack_linearequation_computational")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_condition_number")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_equilibrate")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_error_bound")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_factor")
-cursor.execute("TRUNCATE TABLE lapack_linearequation_invert")      
-cursor.execute("TRUNCATE TABLE lapack_linearequation_solve")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_computational")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_condition_number")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_equilibrate")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_error_bound")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_factor")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_invert")      
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_solve")
 
 
-cursor.execute("TRUNCATE TABLE lapack_linearequation_only")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_only")
 
 
-cursor.execute("TRUNCATE TABLE lapack_routineinfo")
+cursor.execute("TRUNCATE TABLE lighthouse_lapack_le_routineinfo")
 
 conn.commit()
 
