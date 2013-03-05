0624b9723f33692af2d9e0ccf35eeb5a7e984d10
javed <devnull@localhost>
2013-03-05 16:48:56 -0700

    Added anamod files to the sandbox directory.

---
+++ sandbox/anamod/petscmetadataut.h
@@ -0,0 +1,29 @@
+#if !defined(__METADATA_H)
+#define __METADATA_H
+
+#include "petscsnes.h"
+
+#define MAX_STRING 40
+#define METADATA_TABLE_SIZE 10000
+
+typedef struct PetscMetaDataAttribute{char category[MAX_STRING];char event[MAX_STRING];char name[MAX_STRING]; char type[MAX_STRING]; char value[MAX_STRING];}PETSCMETADATAATTRIBUTE;
+
+typedef struct PetscMetadataObject{ PETSCMETADATAATTRIBUTE PetscMetadataTable[METADATA_TABLE_SIZE]; int num;} PETSCMETADATAOBJECT;
+
+PetscErrorCode PetscMetadataInit(void);
+
+PetscErrorCode MetadataSetEvent(char e[]);
+
+PetscErrorCode SNESMonitorMetadata(SNES,PetscInt,PetscReal,void *);
+
+PetscErrorCode AppMetadataInit(void *);
+
+PetscErrorCode AppMonitorMetadata(SNES, PetscInt, PetscReal, void *);
+
+PetscErrorCode AppMetadataFini(void);
+
+PetscErrorCode PetscMetadataFini(void);
+
+PetscErrorCode MatComputePropertiesVariance(Mat, char*);
+
+#endif
