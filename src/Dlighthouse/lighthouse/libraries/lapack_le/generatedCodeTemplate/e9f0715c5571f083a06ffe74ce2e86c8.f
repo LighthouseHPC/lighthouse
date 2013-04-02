a22e1aa4cc0ddfbb605cc6bdab35513680886911
Sa-Lin <devnull@localhost>
2013-04-02 14:56:53 -0500

    removed generatedCodeTemplate/.

--- src/Dlighthouse/lighthouse/libraries/lapack_le/generatedCodeTemplate/e9f0715c5571f083a06ffe74ce2e86c8.f
+++
@@ -1,6 +0,0 @@
-cgesvx(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
-chetrf(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
-dgecon(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
-dgecon(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
-cgecon(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
-zgecon(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
--- src/Dlighthouse/lighthouse/libraries/lapack_le/generatedCodeTemplate/f678e92b8bba7a98f400bc14135d414e.f
+++
@@ -1,1 +0,0 @@
-sgerfs(n, nrhs, &a[0][0], lda, ipiv, &b[0][0], ldb, &info)
