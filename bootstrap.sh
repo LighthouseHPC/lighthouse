a18f19e9d81063ee4b8715659ab481c09d5ff6e9
Luke Groeninger <luke.groeninger@gmail.com>
2013-06-27 11:46:02 -0600

    update bootstrap.sh for compatibility with djangostack

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,7 @@ sudo $pythonexec django_dajaxice
 echo "Installing Dajax"
 sudo $pythonexec django_dajax
 echo "Installing django_taggit"
-sudo easy_install django_taggit
+sudo $pythonexec django_taggit
 
 # No longer needed for now (while using the sqlite3 db for demo purposes)
 #echo ""; echo "--------------------------------------------------------"; 
