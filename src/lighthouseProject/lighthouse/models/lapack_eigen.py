d9bc27413bc8e3d31dc31abaaf206015d52c00b8
Sa-Lin <devnull@localhost>
2014-01-28 15:37:57 -0600

    changed EPROB to EIGEN in models.

--- src/lighthouseProject/lighthouse/models/lapack_eigen.py
+++ src/lighthouseProject/lighthouse/models/lapack_eigen.py
@@ -9,40 +9,40 @@ from collections import OrderedDict
 #
 #
 
-EPROB_PROBLEM_CHOICES = (
+EIGEN_PROBLEM_CHOICES = (
     (u'eval'                 ,u'Eigenproblem'),
     (u'svd'                  ,u'Singular Value Decomposition')
 )
 
-EPROB_NOYES_CHOICES = (
+EIGEN_NOYES_CHOICES = (
     (u'n'    ,u'No'),
     (u'y'    ,u'Yes')
 )
 
-EPROB_YESNO_CHOICES = (
+EIGEN_YESNO_CHOICES = (
     (u'y'    ,u'Yes'),
     (u'n'    ,u'No')
 )
 
-EPROB_MATRIX_CHOICES = (
+EIGEN_MATRIX_CHOICES = (
     (u'gen',u'General'),
     (u'sym',u'Symmetric'),
     (u'her',u'Hermitian'),   
 )
 
-EPROB_STORAGE_CHOICES = (
+EIGEN_STORAGE_CHOICES = (
     (u'full',u'Full'),
     (u'band',u'Band'),
     (u'pack',u'Packed'),
     (u'tri',u'Tridiagonal'),
 )
 
-EPROB_PRECISION_CHOICES = (
+EIGEN_PRECISION_CHOICES = (
     (u's',u'Single'),
     (u'd',u'Double'),
 )
 
-EPROB_ALGORITHM_CHOICES = (
+EIGEN_ALGORITHM_CHOICES = (
     (u'n',u'Default algorithm'),
     (u'dc',u'Divide and Conquer'),
     (u'rrr',u'Relatively Robust Representation'),
@@ -56,17 +56,17 @@ EPROB_ALGORITHM_CHOICES = (
 #
 
 eigen_fields = OrderedDict([
-        ('generalized' , ('Do you need to solve a generalized eigenproblem?', EPROB_NOYES_CHOICES)),
-        ('problem' , ('What type of problem do you need to solve?', EPROB_PROBLEM_CHOICES)),
-        ('complex' , ('Does your matrix have any complex numbers?', EPROB_NOYES_CHOICES)),
-        ('matrixType' , ('What type of matrix do you have?', EPROB_MATRIX_CHOICES)),
-        ('storageType' , ('How is your matrix stored?', EPROB_STORAGE_CHOICES)),
-        ('schur' , ('Do you need the Schur form?', EPROB_NOYES_CHOICES)),
-        ('evaluerange' , ('Do you only need eigenvalues within a specific range?', EPROB_NOYES_CHOICES)),
-        ('algorithm' , ('Do you need to use an advanced algorithm?', EPROB_ALGORITHM_CHOICES)),
-        ('balancing' , ('Do you need a balancing transform or reciprocal condition numbers?', EPROB_NOYES_CHOICES)),
-        ('schurform' , ('Do you need the ordered Schur form or reciprocal condition numbers?', EPROB_NOYES_CHOICES)),
-        ('queryPrecision' , ('Is your matrix single or double precision?', EPROB_PRECISION_CHOICES)),
+        ('generalized' , ('Do you need to solve a generalized eigenproblem?', EIGEN_NOYES_CHOICES)),
+        ('problem' , ('What type of problem do you need to solve?', EIGEN_PROBLEM_CHOICES)),
+        ('complex' , ('Does your matrix have any complex numbers?', EIGEN_NOYES_CHOICES)),
+        ('matrixType' , ('What type of matrix do you have?', EIGEN_MATRIX_CHOICES)),
+        ('storageType' , ('How is your matrix stored?', EIGEN_STORAGE_CHOICES)),
+        ('schur' , ('Do you need the Schur form?', EIGEN_NOYES_CHOICES)),
+        ('evaluerange' , ('Do you only need eigenvalues within a specific range?', EIGEN_NOYES_CHOICES)),
+        ('algorithm' , ('Do you need to use an advanced algorithm?', EIGEN_ALGORITHM_CHOICES)),
+        ('balancing' , ('Do you need a balancing transform or reciprocal condition numbers?', EIGEN_NOYES_CHOICES)),
+        ('schurform' , ('Do you need the ordered Schur form or reciprocal condition numbers?', EIGEN_NOYES_CHOICES)),
+        ('queryPrecision' , ('Is your matrix single or double precision?', EIGEN_PRECISION_CHOICES)),
     ])
 
 
@@ -143,17 +143,17 @@ eigen_advanced_nextform = OrderedDict([
 #
 
 class lapack_eigen(models.Model):
-    generalized = models.CharField('Generalized', max_length=1, choices=EPROB_YESNO_CHOICES)
-    problem = models.CharField('Problem Kind', max_length=4, choices=EPROB_PROBLEM_CHOICES)
-    complex = models.CharField('Complex Numbers', max_length=1, choices=EPROB_YESNO_CHOICES)
-    matrixType = models.CharField('Matrix Type', max_length=3, choices=EPROB_MATRIX_CHOICES)
-    storageType = models.CharField('Matrix Storage', max_length=4, choices=EPROB_STORAGE_CHOICES, blank = True, null = True)
-    schur = models.CharField('Schur Form', max_length=1, choices=EPROB_YESNO_CHOICES, blank = True, null = True)
-    evaluerange = models.CharField('Range of Eigenvalues', max_length=1, choices=EPROB_YESNO_CHOICES, blank= True, null = True)
-    algorithm = models.CharField('Algorithm Used', max_length=4, choices=EPROB_ALGORITHM_CHOICES, blank = True, null = True)
-    balancing = models.CharField('Balancing Transform', max_length=1, choices=EPROB_YESNO_CHOICES, blank = True, null = True)
-    schurform = models.CharField('Ordered Schur Form', max_length=1, choices=EPROB_YESNO_CHOICES, blank = True, null = True)
-    queryPrecision = models.CharField('Number Precision', max_length=1, choices=EPROB_PRECISION_CHOICES)
+    generalized = models.CharField('Generalized', max_length=1, choices=EIGEN_YESNO_CHOICES)
+    problem = models.CharField('Problem Kind', max_length=4, choices=EIGEN_PROBLEM_CHOICES)
+    complex = models.CharField('Complex Numbers', max_length=1, choices=EIGEN_YESNO_CHOICES)
+    matrixType = models.CharField('Matrix Type', max_length=3, choices=EIGEN_MATRIX_CHOICES)
+    storageType = models.CharField('Matrix Storage', max_length=4, choices=EIGEN_STORAGE_CHOICES, blank = True, null = True)
+    schur = models.CharField('Schur Form', max_length=1, choices=EIGEN_YESNO_CHOICES, blank = True, null = True)
+    evaluerange = models.CharField('Range of Eigenvalues', max_length=1, choices=EIGEN_YESNO_CHOICES, blank= True, null = True)
+    algorithm = models.CharField('Algorithm Used', max_length=4, choices=EIGEN_ALGORITHM_CHOICES, blank = True, null = True)
+    balancing = models.CharField('Balancing Transform', max_length=1, choices=EIGEN_YESNO_CHOICES, blank = True, null = True)
+    schurform = models.CharField('Ordered Schur Form', max_length=1, choices=EIGEN_YESNO_CHOICES, blank = True, null = True)
+    queryPrecision = models.CharField('Number Precision', max_length=1, choices=EIGEN_PRECISION_CHOICES)
     thePrecision = models.CharField('Number Precision', max_length=1)
     routineName = models.CharField('Routine Name', max_length=8)
     url = models.URLField('Function URL', blank = True, null = True)
