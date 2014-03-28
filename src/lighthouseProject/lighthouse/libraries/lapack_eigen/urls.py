ab7499d9c38aa742c8d59e2c13054fff0c18c336
Sa-Lin <devnull@localhost>
2014-03-28 18:37:52 -0500

    updated eigen url.

--- src/lighthouseProject/lighthouse/libraries/lapack_eigen/urls.py
+++ src/lighthouseProject/lighthouse/libraries/lapack_eigen/urls.py
@@ -3,7 +3,10 @@ from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
 urlpatterns = patterns('lighthouse.views.lapack_eigen',
-    url(r'^$', "lapack_eigen", name="index"),
-    url(r'^clear_session/$', "eigen_clear_session"),  
-    url(r'^update_session/$', "eigen_update_session"),
+    url(r'^$', "guidedSearch_problem", name="guidedSearch_problem"),
+    url(r'^complex/$', "guidedSearch_complex", name="guidedSearch_complex"),
+    #url(r'^matrixType/$', "guidedSearch_complex", name="guidedSearch_complex"),
+    #url(r'^$', "lapack_eigen", name="index"),
+    #url(r'^clear_session/$', "eigen_clear_session"),  
+    #url(r'^update_session/$', "eigen_update_session"),
 )
