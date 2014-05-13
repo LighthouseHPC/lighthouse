b7d8277df75d33b8b208c44ed4d0fe10358210be
Sa-Lin <devnull@localhost>
2014-05-13 16:00:59 -0500

    modified libraries/RoutineInfo/lapack/load_data.py to allow for inserting data starting from any id.

--- src/lighthouseProject/lighthouse/libraries/RoutineInfo/lapack/load_data.py
+++ src/lighthouseProject/lighthouse/libraries/RoutineInfo/lapack/load_data.py
@@ -8,7 +8,16 @@ cursor = myDB.cursor()
 
 reader = csv.reader(open("routine_info.csv"))
 
-i = 1
+ID = raw_input('To load all data, enter "all"; otherwise, enter the ID number you wish to start loading:	')
+
+print 'Your input is ' + ID
+
+
+if ID == 'all':
+	i = 1
+else:
+	i = int(ID)
+	
 for file in reader:
 	fd = open("RoutineTxt/"+file[0], "r")
 	myDB.query("insert into lighthouse_lapack_routineinfo (id, routine, info) values (\'%d\', \'%s\', \'%s\')" % (i, file[0], myDB.escape_string(fd.read())))
