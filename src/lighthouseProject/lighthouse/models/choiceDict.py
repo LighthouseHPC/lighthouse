be4738de47a190f45e4306f755df0508bd308e2f
Sa-Lin <devnull@localhost>
2014-05-13 16:27:48 -0500

    organized models/choiceDict.py.

--- src/lighthouseProject/lighthouse/models/choiceDict.py
+++ src/lighthouseProject/lighthouse/models/choiceDict.py
@@ -1,31 +1,4 @@
-PRECISION_CHOICES = (
-        (u's',                          u'single'), 
-        (u'd',                          u'double'), 
-        (u'c',                          u'complex'), 
-        (u'z',                          u'complex double'),
-)
-
-
-SINGLEDOUBLE_CHOICES = (
-        (u'single',                          u'single'), 
-        (u'double',                          u'double'),        
-)
-
-
-EIGENPROBLEM_CHOICES = (
-	(u'eigen',				u'Solve an eigenproblem'),
-	(u'Hessenberg',				u'Reduce a matrix to upper Hessenberg form'),
-	(u'cndNumber_of_evtrs',			u'Estimate condition numbers of eigenvectors'),
-	(u'balance',				u'Balance a general matrix to improve eigenvalue accuracy'),
-)
-
-
-STANDARD_CHOICES = (
-        (u'standard',                   u'standard'),
-        (u'generalized',                u'generalized'), 
-)
-
-
+#####----------- for all LAPACK routines -------------#####
 MATRIX_CHOICES = (
         (u'general',                    u'general'), 
         (u'symmetric',                  u'symmetric'), 
@@ -33,6 +6,7 @@ MATRIX_CHOICES = (
         (u'SPD',                        u'SPD'),
         (u'HPD',                        u'HPD'),
         (u'triangular',                 u'triangular'),
+        (u'upper triangular',           u'upper triangular'),
         (u'SPsD',                       u'SPsD'),
         (u'HPsD',                       u'HPsD'),
         (u'upper Hessenberg',           u'upper Hessenberg'),
@@ -45,8 +19,23 @@ STORAGE_CHOICES = (
         (u'band',                       u'band'),
         (u'packed',                     u'packed'),
         (u'tridiagonal',                u'tridiagonal'),
+        (u'bidiagonal',                 u'bidiagonal'),
         (u'RFP',                        u'RFP'),
         (u'full/packed/band/tridiagonal', u'full/packed/band/tridiagonal'),
+)
+
+
+PRECISION_CHOICES = (
+        (u's',                          u'single'), 
+        (u'd',                          u'double'), 
+        (u'c',                          u'complex'), 
+        (u'z',                          u'complex double'),
+)
+
+
+SINGLEDOUBLE_CHOICES = (
+        (u'single',                          u'single'), 
+        (u'double',                          u'double'),        
 )
 
 
@@ -67,4 +56,36 @@ NOYESBOTH_CHOICES = (
         (u'no',         u'no'),
         (u'yes',        u'yes'),
         (u'no/yes',     u'no/yes'), 
+)
+
+
+
+
+
+
+#####----------- for LAPACK eigen, svd routines -------------#####
+STANDARD_CHOICES = (
+        (u'standard',                   u'standard'),
+        (u'generalized',                u'generalized'), 
+)
+
+
+
+
+
+#####----------- for LAPACK eigen routines -------------#####
+EIGENPROBLEM_CHOICES = (
+	(u'eigen',				u'Solve an eigenproblem'),
+	(u'Hessenberg',				u'Reduce a matrix to upper Hessenberg form'),
+	(u'cndNumber_of_evtrs',			u'Estimate condition numbers of eigenvectors'),
+	(u'balance',				u'Balance a general matrix to improve eigenvalue accuracy'),
+)
+
+
+
+
+#####----------- for LAPACK SVD routines -------------#####
+EIGENPROBLEM_CHOICES = (
+	(u'svd',				u'Computes singular value decomposition (SVD)'),
+	(u'bidiagonal',				u'Reduce a matrix to bidiagonal form'),
 )
