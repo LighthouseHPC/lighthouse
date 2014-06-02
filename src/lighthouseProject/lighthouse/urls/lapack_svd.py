b8c32dcb1cce1d88c2ea90585ab06b2c80894a90
Sa-Lin <devnull@localhost>
2014-06-02 14:55:34 -0500

    updated svd urls.py in lighthouseProject.

--- src/lighthouseProject/lighthouse/urls/lapack_svd.py
+++ src/lighthouseProject/lighthouse/urls/lapack_svd.py
@@ -4,17 +4,9 @@ from django.conf import settings
 
 urlpatterns = patterns('lighthouse.views.lapack_svd',
     url(r'^$', "guidedSearch_index", name="guidedSearch_index"),
-    url(r'^problem/$', "guidedSearch_problem", name="guidedSearch_problem"),
-    url(r'^standardGeneralized/$', "guidedSearch_standardGeneralized", name="guidedSearch_standardGeneralized"),
-    url(r'^complexNumber/$', "guidedSearch_complexNumber", name="guidedSearch_complexNumber"),
-    url(r'^matrixType/$', "guidedSearch_matrixType", name="guidedSearch_matrixType"),
-    url(r'^storageType/$', "guidedSearch_storageType", name="guidedSearch_storageType"),
-    url(r'^singularValues/$', "guidedSearch_singularValues", name="guidedSearch_singularValues"),
-    url(r'^singularVectors/$', "guidedSearch_singularVectors", name="guidedSearch_singularVectors"),
-    #url(r'^schur/$', "guidedSearch_schur", name="guidedSearch_schur"),
-    #url(r'^cndNumber/$', "guidedSearch_cndNumber", name="guidedSearch_cndNumber"),
-    url(r'^thePrecision/$', "guidedSearch_thePrecision", name="guidedSearch_thePrecision"),
+    url(r'^index/$', "guidedSearch_index", name="guidedSearch_index"),
+    url(r'^guidedSearch/$', "guidedSearch", name="guidedSearch"),
 
-    #url(r'^clear_session/$', "svd_clear_session"),  
-    #url(r'^update_session/$', "svd_update_session"),
+    #url(r'^clear_session/$', "eigen_clear_session"),  
+    #url(r'^update_session/$', "eigen_update_session"),
 )
