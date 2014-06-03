132efde58da396141a9471f52824701d572c24c0
Sa-Lin <devnull@localhost>
2014-06-03 15:24:39 -0500

    modified svd forms to disable buttons in singularVectorsForm and cleaned up the file.

--- src/lighthouseProject/lighthouse/forms/lapack_svd.py
+++ src/lighthouseProject/lighthouse/forms/lapack_svd.py
@@ -4,31 +4,6 @@ from lighthouse.models.lapack_svd import *
 from lighthouse.models.lapack_choiceDict import *
 from lighthouse.forms.lapack_eigen import CustomRadioSelect
 
-#####------- Allow disabling options in a RadioSelect widget ----------#####
-#from django.utils.safestring import mark_safe
-#from django.utils.encoding import force_unicode
-#
-#class CustomRadioRenderer(forms.widgets.RadioFieldRenderer):
-#    def render(self):
-#        """ Disable some radio buttons based on disableList """
-#	if self.disable == []:
-#	    return mark_safe(u'<ul>\n%s\n</ul>' % u'\n'.join([u'<li>%s</li>' % force_unicode(w) for w in self]))
-#	else:
-#	    midList = []
-#	    for x, wid in enumerate(self):
-#		if self.disable[x] == True:
-#		    wid.attrs['disabled'] = True
-#		midList.append(wid)
-#	    return mark_safe(u'<ul>\n%s\n</ul>' % u'\n'.join([u'<li>%s</li>' % w for w in midList]))
-#
-#
-#class CustomRadioSelect(forms.widgets.RadioSelect):
-#    renderer = CustomRadioRenderer
-    
-    
-    
-    
-    
     
 
 ######-------- For Guided Search --------######
@@ -98,34 +73,28 @@ class storageTypeForm(forms.Form):
     
 ##--- svdvectors form ---##
 class singularVectorsForm(forms.Form):
-    svd_singularVectors = forms.ChoiceField(label='Do you need the singular vectors?',
-						widget=forms.RadioSelect(),
-						choices=NOYES_CHOICES)
-#    def __init__(self, request, *args, **kwargs):
-#	super(singularVectorsForm, self).__init__(*args, **kwargs)
-#	self.fields['svd_singularVectors'].choices = request.session['Routines'].values_list('singularVectors', 'singularVectors').distinct()
-#	disableList = []
-#	##--- if there is only one choice, show the others but disable them ---##
-#	if len(self.fields['svd_singularVectors'].choices) == 1:
-#	    selected = self.fields['svd_singularVectors'].choices[0][1]
-#	    self.fields['svd_singularVectors'].choices = NOYES_CHOICES
-#	    self.fields['svd_singularVectors'].initial = selected
-#	    for item in self.fields['svd_singularVectors'].choices:
-#		if item[1] != selected:
-#		    disableList.append(True)
-#		else:
-#		    disableList.append(False)
-#		    
-#	self.fields['svd_singularVectors'].widget.renderer.disable = disableList
+    svd_singularVectors = forms.ChoiceField(label='Do you need the singular vectors?', choices=NOYES_CHOICES, widget=CustomRadioSelect())
+    def __init__(self, request, *args, **kwargs):
+	super(singularVectorsForm, self).__init__(*args, **kwargs)
+	self.fields['svd_singularVectors'].choices = request.session['Routines'].values_list('singularVectors', 'singularVectors').distinct()
+	disableList = []
+	##--- if there is only one choice, show the others but disable them ---##
+	if len(self.fields['svd_singularVectors'].choices) == 1:
+	    selected = self.fields['svd_singularVectors'].choices[0][1]
+	    self.fields['svd_singularVectors'].choices = NOYES_CHOICES
+	    self.fields['svd_singularVectors'].initial = selected
+	    for item in self.fields['svd_singularVectors'].choices:
+		if item[1] != selected:
+		    disableList.append(True)
+		else:
+		    disableList.append(False)
+	else:
+	    self.fields['svd_singularVectors'].choices = NOYES_CHOICES
+		    
+	self.fields['svd_singularVectors'].widget.renderer.disable = disableList
     
     
-##--- condition number form ---##
-#class cndNumberForm(forms.Form):
-#    svd_cndNumber = forms.ChoiceField(label='Do you need a balancing transformation and/or a reciprocal condition number?',
-#					      widget=forms.RadioSelect(),
-#					      choices=NOYES_CHOICES
-#					      )
-    
+
     
     
 ##--- precision form ---##
