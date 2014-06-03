bf4dd63db03d6e14e4340d912f6f02be04ca00a1
Sa-Lin <devnull@localhost>
2014-06-03 16:10:33 -0500

    added svd models to Dlighthouse.

---
+++ src/Dlighthouse/lighthouse/models/lapack_svd.py
@@ -0,0 +1,29 @@
+from django.db import models
+from lighthouse.models.lapack_choiceDict import *
+from lighthouse.models.lapack_routineInfo import lapack_RoutineInfo
+
+
+
+###---------------- for guided search ----------------###
+###--- SVD ---###
+class lapack_svd(models.Model):
+        thePrecision = models.CharField('Precision', max_length=10, choices=PRECISION_CHOICES)
+        routineName = models.CharField('Routine Name', max_length=30)
+        complexNumber = models.CharField('Complex Number', max_length=10, choices=NOYES_CHOICES)
+        problem = models.CharField('Problem Type', max_length=20, choices=SVD_CHOICES)
+        matrixType = models.CharField('Matrix Type', max_length=30, choices=MATRIX_CHOICES)
+        storageType = models.CharField('Storage', max_length=60, choices=STORAGE_CHOICES)
+        singularVectors = models.CharField('singular vectors', max_length=10, choices=NOYESBOTH_CHOICES)
+        singleDouble = models.CharField('single/double', max_length=10, choices=SINGLEDOUBLE_CHOICES)
+        notes = models.CharField('Notes', max_length=225)
+        info = models.ForeignKey(lapack_RoutineInfo)
+
+        class Admin:
+                list_display = ('id', 'thePrecision', 'routineName', 'matrixType', 'storageType', 'info')
+
+        def __unicode__(self):
+                return self.matrixType
+                return self.storageType
+        
+        class Meta:
+                app_label = 'lighthouse'
