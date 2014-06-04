4318806c33d0f5d844c3faa69baa0a5aaffb1927
Sa-Lin <devnull@localhost>
2014-06-04 14:07:01 -0500

    added slepc_eprob into model __init__.

--- src/Dlighthouse/lighthouse/models/__init__.py
+++ src/Dlighthouse/lighthouse/models/__init__.py
@@ -1,4 +1,4 @@
 from lighthouse.models.lapack_le import *
 from lighthouse.models.lapack_eigen import *
 from lighthouse.models.lapack_svd import *
-#from lighthouse.models.lapack_eprob import *
+from lighthouse.models.slepc_eprob import *
