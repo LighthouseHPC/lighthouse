6ec0e093ebcf9c3b85248fdb2e5df995129ae582
Sa-Lin <devnull@localhost>
2014-06-04 11:25:21 -0500

    changed the database link for lapack_svd in lighthouseProject.

--- src/lighthouseProject/lighthouse/database/lapack_svd/dropTables.py
+++ src/lighthouseProject/lighthouse/database/lapack_svd/dropTables.py
@@ -1,14 +1,10 @@
 import MySQLdb
 
-conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="yellow1234", db="shark") 
+conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="lighthouse", passwd="lighthousedb", db="shark") 
 
 cursor = conn.cursor()
 
 cursor.execute("DROP TABLE IF EXISTS lighthouse_lapack_eigen")
-
-cursor.execute("DROP TABLE IF EXISTS lighthouse_lapack_sylvester")
-
-cursor.execute("DROP TABLE IF EXISTS lighthouse_lapack_routineinfo")
 
 conn.commit()
 
