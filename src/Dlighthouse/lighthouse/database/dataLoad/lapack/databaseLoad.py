ebec0a553e1bd3ad6645d929bf3369680577f3ad
Sa-Lin <devnull@localhost>
2014-06-04 15:28:05 -0500

    fixed the errors in databaseLoad.py.

--- src/Dlighthouse/lighthouse/database/dataLoad/lapack/databaseLoad.py
+++ src/Dlighthouse/lighthouse/database/dataLoad/lapack/databaseLoad.py
@@ -17,10 +17,11 @@ os.chdir(database_path)
 
 
 
+
 ##### ------------ for LAPACK_le tables ---------------- #####
 ### load data to table
 ### lapack_le_linearequation_simple, lapack_le_linearequation_expert, lapack_le_linearequation_driver
-os.chdir(database_path+"/lapack_le/databaseMng/Driver")
+os.chdir(database_path+"/lapack_le/Driver")
 os.system('python load_data.py')
 os.chdir(database_path)
 
@@ -30,21 +31,21 @@ os.chdir(database_path)
 ### lapack_le_linearequation_solve, lapack_le_linearequation_condition_number
 ### lapack_le_linearequation_error_bound, lapack_le_linearequation_invert
 ### lapack_le_linearequation_equilibrate
-os.chdir(database_path+"/lapack_le/databaseMng/Computational")
+os.chdir(database_path+"/lapack_le/Computational")
 os.system('python load_data.py')
 os.chdir(database_path)
 
 
 ### load data to table
 ### lapack_le_only
-os.chdir(database_path+"/lapack_le/databaseMng/Combine")
+os.chdir(database_path+"/lapack_le/Combine")
 os.system('python load_data.py')
 os.chdir(database_path)
 
 
 ### load data to table
 ### lapack_le_arg
-os.chdir(database_path+"/lapack_le/databaseMng/Arguments")
+os.chdir(database_path+"/lapack_le/Arguments")
 os.system('python load_data.py')
 os.chdir(database_path)
 
