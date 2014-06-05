983cba178585a9a8f7f5a15a1240864d9f70d666
Sa-Lin <devnull@localhost>
2014-06-05 14:59:03 -0500

    uncommented load_data.py for lapack_le computational in Dlighthouse.

--- src/Dlighthouse/lighthouse/database/lapack_le/Computational/load_data.py
+++ src/Dlighthouse/lighthouse/database/lapack_le/Computational/load_data.py
@@ -15,28 +15,28 @@ nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_computational_a
                                      TABLE lighthouse_lapack_le_computational FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
-#nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_factor.csv' INTO \
-#                                     TABLE lighthouse_lapack_le_factor FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_factor.csv' INTO \
+                                     TABLE lighthouse_lapack_le_factor FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
 nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_solve.csv' INTO \
                                      TABLE lighthouse_lapack_le_solve FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
-#nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_condition_number.csv' INTO \
-#                                     TABLE lighthouse_lapack_le_condition_number FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_condition_number.csv' INTO \
+                                     TABLE lighthouse_lapack_le_condition_number FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
-#nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_error_bounds.csv' INTO \
-#                                     TABLE lighthouse_lapack_le_error_bound FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_error_bounds.csv' INTO \
+                                     TABLE lighthouse_lapack_le_error_bound FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
-#nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_invert.csv' INTO \
-#                                     TABLE lighthouse_lapack_le_inverse FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_invert.csv' INTO \
+                                     TABLE lighthouse_lapack_le_inverse FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
-#nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_equilibrate.csv' INTO \
-#                                     TABLE lighthouse_lapack_le_equilibrate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+nr_records_inserted = cursor.execute("LOAD DATA LOCAL INFILE 'le_equilibrate.csv' INTO \
+                                     TABLE lighthouse_lapack_le_equilibrate FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
 
 
 
