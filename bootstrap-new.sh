0ec78693f22abef3b249ac90b53bdf1715e5c9c7
Jeffrey Cook <cookjj@colorado.edu>
2014-06-05 15:39:47 -0600

    New universal bootstrap-new.sh script Restoring views/lapack_le.py that I may have accidentally obliterated before

---
+++ bootstrap-new.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Make sure executed from djangostack
+if [[ $PATH != *djangostack* ]]
+then
+  echo "ERROR: Please run ./use_djangostack from your installation of"
+  echo "       djangostack before running the bootstrap! Exiting."
+  exit 1  
+fi
+
+pythonexec=easy_install
+pythonexec2=python
+
+echo "Using python $pythonexec2"
+
+echo "Installing Whoosh"
+$pythonexec "whoosh==2.5.5"
+echo "Installing Haystack"
+$pythonexec "django-haystack==2.1"
+echo "Installing Dajaxice"
+$pythonexec django_dajaxice
+echo "Installing Dajax"
+$pythonexec django_dajax
+echo "Installing django_taggit"
+$pythonexec django_taggit
+echo "Installing django-extensions"
+$pythonexec django-extensions
+
+echo " "
+echo "(Please run 'python manage.py syncdb' to seed database if necessary.)"
+echo " "
+echo "(Run 'python databaseLoad.py' from src/Dlighthouse/lighthouse/libraries/lapack_le/databaseMng/ if necessary.)"
+
