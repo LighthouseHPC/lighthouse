f808345c7bd5c4ff031f67dd9d548d5f33a8532c
Sa-Lin <devnull@localhost>
2014-06-16 20:52:06 -0500

    changed lapack urls names

--- src/lighthouseProject/lighthouse/urls/lapack_eigen.py
+++ src/lighthouseProject/lighthouse/urls/lapack_eigen.py
@@ -3,9 +3,9 @@ from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
 urlpatterns = patterns('lighthouse.views.lapack_eigen',
-    url(r'^$', "guidedSearch_index", name="guidedSearch_index"),
-    url(r'^index/$', "guidedSearch_index", name="guidedSearch_index"),
-    url(r'^guidedSearch/$', "guidedSearch", name="guidedSearch"),
+    url(r'^$', "guidedSearch_index", name="lapack_eigen"),
+    url(r'^index/$', "guidedSearch_index", name="lapack_eigen"),
+    url(r'^guidedSearch/$', "guidedSearch", name="lapack_eigen_guidedSearch"),
 
     #url(r'^clear_session/$', "eigen_clear_session"),  
     #url(r'^update_session/$', "eigen_update_session"),
