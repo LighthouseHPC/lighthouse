137b8e08486cc24bc81c66577866986067352c35
Sa-Lin <devnull@localhost>
2014-06-19 15:35:13 -0500

    fixed lighthouseProject top urls.py.

--- src/lighthouseProject/lighthouseProject/urls.py
+++ src/lighthouseProject/lighthouseProject/urls.py
@@ -1,43 +1,44 @@
-from django.conf.urls.defaults import *
-from Dlighthouse import settings
-
+from django.conf.urls import patterns, include, url
 
 ### Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-### To enable dajaxice
+
+### Configure dajaxice url
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 dajaxice_autodiscover()
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+### static files
+from django.conf import settings
+from django.conf.urls.static import static
 
 ### Straight to template
 from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
-    ### Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    ### to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Examples:
+    # url(r'^$', 'lighthouseProject.views.home', name='home'),
+    # url(r'^lighthouseProject/', include('lighthouseProject.foo.urls')),
 
-    ### For dojango
-    (r'^dojango/', include('dojango.urls')),
-
-    ### For Haystack
-    #(r'^search/', include('haystack.urls')),
-    
-    
-    ###  For dajaxice
-    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     ### Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-
-    ### Use the files in the media directory:
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^admin/', include(admin.site.urls)),
     
-    ### Use the files in the templates dirctory
-    (r'^templates/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.TEMPLATE_ROOT}),  
+    ### for dajaxcie
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    
+    ### for dojango
+    (r'^dojango/', include('dojango.urls')),
+
+    #### Use the files in the media directory:
+    #(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    #
+    #### Use the files in the templates dirctory
+    #(r'^templates/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.TEMPLATE_ROOT}),  
     
     ### Go to the login page
     url(r'^$', 'django.contrib.auth.views.login', name="login"),
@@ -59,26 +60,22 @@ urlpatterns = patterns('',
     ### Link forum/urls.py for blog:
     (r'^forum/', include('forum.urls')),
     
+    
     ###----------------- for LAPACK -----------------------###
-    ### for LAPACK routines -- link lighthouse/urls/lapack_*.py:
-    url(r'^lapack_le/', include('lighthouse.urls.lapack_le')),
+    #url(r'^lapack_le/', include('lighthouse.urls.lapack_le')),
     url(r'^lapack_eigen/', include('lighthouse.urls.lapack_eigen')),
     url(r'^lapack_svd/', include('lighthouse.urls.lapack_svd')),
     url(r'^lapack_sylvester/', include('lighthouse.urls.lapack_sylvester')),
-    url(r'^lapack_eprob/', include('lighthouse.urls.lapack_eprob')),
 
     ###----------------- for PETSc -----------------------###
-    ### for PETSc routines -- link lighthouse/urls/lapack_*.py:
-    url(r'^petsc/', include('lighthouse.urls.petsc_le')),
+    #url(r'^petsc/', include('lighthouse.urls.petsc_le')),
 
     ###----------------- for SLEPc -----------------------###
-    ### for SLEPc routines -- link lighthouse/urls/lapack_*.py:
-    url(r'^slepc_eprob/', include('lighthouse.urls.slepc_eprob')),
+    #url(r'^slepc_eprob/', include('lighthouse.urls.slepc_eprob')),
 
     ###----------------- for BTO script ------------------###
     ### ###
-    url(r'^btoscript/', include('lighthouse.urls.btoscript'))
-
-)
-
-urlpatterns += staticfiles_urlpatterns()
+    #url(r'^btoscript/', include('lighthouse.urls.btoscript'))
+    
+    
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
