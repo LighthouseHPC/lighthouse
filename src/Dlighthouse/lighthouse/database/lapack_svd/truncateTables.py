aa28d1a8baebab0b4c0f7b79a9dbc947eaeb671a
Sa-Lin <devnull@localhost>
2014-07-14 17:44:32 -0500

    reorganized lapack_svd database and changed routineinfo id.

--- src/Dlighthouse/lighthouse/database/lapack_svd/truncateTables.py
+++ src/Dlighthouse/lighthouse/database/lapack_svd/truncateTables.py
@@ -1,12 +1,10 @@
 import MySQLdb
 
-conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="yellow1234", db="shark") 
+conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="lighthouse", passwd="lighthousedb", db="shark") 
 
 cursor = conn.cursor()
 
 cursor.execute("TRUNCATE TABLE lighthouse_lapack_eigen")
-
-cursor.execute("TRUNCATE TABLE lighthouse_lapack_sylvester")
 
 conn.commit()
 
