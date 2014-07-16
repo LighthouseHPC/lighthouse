2459ce0391b2c16530f81b8909d8b5e6800fabec
Sa-Lin <devnull@localhost>
2014-07-16 14:11:53 -0500

    named btoscript url.

--- src/Dlighthouse/lighthouse/urls/btoscript.py
+++ src/Dlighthouse/lighthouse/urls/btoscript.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 urlpatterns = patterns('lighthouse.views.btoscript',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name="btoscript"),
     url(r'^args/$', 'args'),
 #    url(r'^codes/$', 'codes'),
 )
