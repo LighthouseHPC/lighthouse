c80f3aed6734836f6b8d9eac96b3edc1ce90f9e1
Sa-Lin <devnull@localhost>
2014-08-01 16:08:42 -0500

    inserted 'description' field to lapack_svd adcanved model and updated the database and the template.

--- src/Dlighthouse/lighthouse/models/lapack_svd.py
+++ src/Dlighthouse/lighthouse/models/lapack_svd.py
@@ -35,7 +35,8 @@ class lapack_svd_advanced(models.Model):
         routineName = models.CharField('Routine Name', max_length=30)
         driverComput = models.CharField('driver/computational', max_length=15, choices=DRIVERCOMPUT_CHOICES)
         standardGeneralized = models.CharField('Standard/Generalized', max_length=20, choices=STANDARD_CHOICES)
-        function = models.CharField('Function Type', max_length=20, choices=FUNCTION_CHOICES)
+        function = models.CharField('Function', max_length=20, choices=FUNCTION_CHOICES)
+        description = models.CharField('Description', max_length=100)
         method = models.CharField('Method', max_length=20, choices=METHOD_CHOICES)
         complexNumber = models.CharField('Complex Number', max_length=10, choices=NOYES_CHOICES)
         matrixType = models.CharField('Matrix Type', max_length=30, choices=MATRIX_CHOICES)
@@ -48,6 +49,7 @@ class lapack_svd_advanced(models.Model):
                 list_display = ('id', 'thePrecision', 'routineName', 'matrixType', 'storageType', 'info')
 
         def __unicode__(self):
+                return self.routineName
                 return self.matrixType
                 return self.storageType
         
--- src/lighthouseProject/lighthouse/database/lapack_svd/dropTables.py
+++ src/lighthouseProject/lighthouse/database/lapack_svd/dropTables.py
@@ -1,10 +1,10 @@
 import MySQLdb
 
-conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="lighthouse", passwd="lighthousedb", db="shark") 
+conn = MySQLdb.connect(host="127.0.0.1", port=3306, user="lighthouse", passwd="yellow1234", db="lighthousedb") 
 
 cursor = conn.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS lighthouse_lapack_eigen")
+cursor.execute("DROP TABLE IF EXISTS lighthouse_lapack_svd_advanced")
 
 conn.commit()
 
