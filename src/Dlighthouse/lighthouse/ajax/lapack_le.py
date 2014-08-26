0c317af356be1a8cd6762fd0e8fc7c4c24fa5510
Sa-Lin <devnull@localhost>
2014-08-26 12:53:59 -0500

    fixed the code template problem.

--- src/Dlighthouse/lighthouse/ajax/lapack_le.py
+++ src/Dlighthouse/lighthouse/ajax/lapack_le.py
@@ -14,42 +14,41 @@ extension_dic = {'fortran': 'f90', 'cpp': 'c'}
 def createTemplate(request, checked_list, language, time):
 	dajax = Dajax()
 	dajax.add_css_class("#template_output", "brush: %s;"%language)
-	#try:
-	extension = extension_dic[language]
-	file_zip = zipfile.ZipFile(dir_download+"lighthouse_%s.zip"%time, "w")
-	makeFile("temp_%s.%s"%(checked_list[0].lower(), extension))
-	with open(dir_download+'%s.%s'%(time, extension), 'w') as outfile:
-		for item in checked_list:
-			item = item.lower()
-			if language == 'cpp':
-				go = generateTemplate_C(item)
-			else:
-				go = generateTemplate(item)
-			go.make_template()
-			name = "./lighthouse/templateGen/lapack_le/%s/codeTemplates/temp_%s.%s"%(language, item, extension)
-			file_zip.write(name, os.path.basename(name), zipfile.ZIP_DEFLATED)
-			with open(name,"r") as infile:
-				outfile.write(infile.read())
-				
-	## display f_output, which contains all routines
-	f_output = open(dir_download+"%s.%s"%(time, extension),"r")
-	dajax.assign("#template_output", 'innerHTML', f_output.read())
-	dajax.script('SyntaxHighlighter.highlight()')
-	f_output.close()
-	
-	## write README into the zip file
-	file_zip.write("./lighthouse/templateGen/lapack_le/README", os.path.basename(dir_download+"README"), zipfile.ZIP_DEFLATED)
-	
-	## write makefile into the zip file
-	file_zip.write(dir_download+"makefile", os.path.basename(dir_download+"makefile"), zipfile.ZIP_DEFLATED)
-	file_zip.close()
-	
-	## remove makefile from the download directory
-	os.remove(dir_download+"makefile")
+	try:
+		extension = extension_dic[language]
+		file_zip = zipfile.ZipFile(dir_download+"lighthouse_%s.zip"%time, "w")
+		makeFile("temp_%s.%s"%(checked_list[0].lower(), extension))
+		with open(dir_download+'%s.%s'%(time, extension), 'w') as outfile:
+			for item in checked_list:
+				item = item.lower()
+				if language == 'cpp':
+					go = generateTemplate_C(item)
+				else:
+					go = generateTemplate(item)
+				go.make_template()
+				name = "./lighthouse/templateGen/lapack_le/%s/codeTemplates/temp_%s.%s"%(language, item, extension)
+				file_zip.write(name, os.path.basename(name), zipfile.ZIP_DEFLATED)
+				with open(name,"r") as infile:
+					outfile.write(infile.read())
+					
+		## display f_output, which contains all routines
+		f_output = open(dir_download+"%s.%s"%(time, extension),"r")
+		dajax.assign("#template_output", 'innerHTML', f_output.read())
+		dajax.script('SyntaxHighlighter.highlight()')
+		f_output.close()
 		
-	#except:
-	#	dajax.assign("#template_output", 'innerHTML', 'Coming soon...')
-	
+		## write README into the zip file
+		file_zip.write("./lighthouse/templateGen/lapack_le/README", os.path.basename(dir_download+"README"), zipfile.ZIP_DEFLATED)
+		
+		## write makefile into the zip file
+		file_zip.write(dir_download+"makefile", os.path.basename(dir_download+"makefile"), zipfile.ZIP_DEFLATED)
+		file_zip.close()
+		
+		## remove makefile from the download directory
+		os.remove(dir_download+"makefile")
+		
+	except:
+		dajax.assign("#template_output", 'innerHTML', 'Coming soon...')		
 
 	return dajax.json()
 	
