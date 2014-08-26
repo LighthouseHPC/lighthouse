b214440d3f4e7a3abfa3f3afe9d59a3735edb65d
Sa-Lin <devnull@localhost>
2014-08-26 14:01:07 -0500

    found petsc_le/work_dir/.

---
+++ src/Dlighthouse/lighthouse/templateGen/petsc_le/work_dir/linear_solver.c
@@ -0,0 +1,131 @@
+
+/* Program usage:  mpiexec ex1 [-help] [all PETSc options] */
+
+static char help[] = "Solves a linear system with KSP.\n\n";
+
+/*T
+   Main operation: Solve a linear system
+   Input file format: PETSc binary format (matrix and rhs in the same file)
+   Processor: 1 (sequential)
+   Output format: PETSc binary format
+T*/
+
+#include <petscksp.h>
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **args)
+{
+  Vec            x, b;      /* approx solution, RHS, exact solution */
+  Mat            A;        /* linear system matrix */
+  KSP            ksp;          /* linear solver context */
+  PetscViewer    fd,viewer;
+  PetscErrorCode ierr;
+  PetscInt       its;
+  PetscMPIInt    size;
+  char           file[2][PETSC_MAX_PATH_LEN];     /* input file name */
+  PetscBool      flg;
+
+  PetscInitialize(&argc,&args,(char *)0,help);
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
+  if (size != 1) SETERRQ(PETSC_COMM_WORLD,1,"This is a uniprocessor example only!");
+
+  /* 
+     Determine files from which we read the linear system (matrix and right-hand-side vector).
+  */
+  ierr = PetscOptionsGetString(PETSC_NULL,"-f",file[0],PETSC_MAX_PATH_LEN,&flg);CHKERRQ(ierr);
+  if (!flg) {
+    SETERRQ(PETSC_COMM_WORLD,1,"Must indicate binary file with the -f option");
+  }
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+         Compute the matrix and right-hand-side vector that define
+         the linear system, Ax = b.
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /* 
+       Open binary file.  Note that we use FILE_MODE_READ to indicate
+       reading from this file.
+  */
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,file[0],FILE_MODE_READ,&fd);CHKERRQ(ierr);
+  
+  /*
+     Load the matrix and vector; then destroy the viewer.
+  */
+  ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatLoad(A,fd);CHKERRQ(ierr);
+
+  ierr = VecCreate(PETSC_COMM_WORLD,&b);CHKERRQ(ierr);
+  ierr = VecSetFromOptions(b);CHKERRQ(ierr);
+  ierr = VecLoad(b,fd);CHKERRQ(ierr);
+
+  ierr = VecDuplicate(b,&x);CHKERRQ(ierr);
+
+  ierr = PetscViewerDestroy(&fd);CHKERRQ(ierr); 
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                Create the linear solver and set various options
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /* 
+     Create linear solver context
+  */
+  ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
+
+  /* 
+     Set operators. Here the matrix that defines the linear system
+     also serves as the preconditioning matrix.
+  */
+  ierr = KSPSetOperators(ksp,A,A,DIFFERENT_NONZERO_PATTERN);CHKERRQ(ierr);
+
+  /* 
+    Set runtime options, e.g.,
+        -ksp_type <type> -pc_type <type> -ksp_monitor -ksp_rtol <rtol>
+    These options will override those specified above as long as
+    KSPSetFromOptions() is called _after_ any other customization
+    routines.
+  */
+  ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);  
+ 
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                      Solve the linear system
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /* 
+     Solve linear system
+  */
+  ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr); 
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+                      Check solution and clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /* 
+     Check the error
+  */
+  
+  ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Iterations %D\n",its);CHKERRQ(ierr);
+  //VecView(x,PETSC_VIEWER_STDOUT_WORLD);
+
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,"solution.petsc",FILE_MODE_WRITE,&viewer);CHKERRQ(ierr);
+  ierr = VecView(x, viewer);CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+
+  /* 
+     Free work space.  All PETSc objects should be destroyed when they
+     are no longer needed.
+  */
+  ierr = VecDestroy(&x);CHKERRQ(ierr);
+  ierr = VecDestroy(&b);CHKERRQ(ierr); 
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
+
+  /*
+     Always call PetscFinalize() before exiting a program.  This routine
+       - finalizes the PETSc libraries as well as MPI
+       - provides summary and diagnostic information if certain runtime
+         options are chosen (e.g., -log_summary).
+  */
+
+  ierr = PetscFinalize();
+  return 0;
+}
