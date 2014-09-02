e20d456b87916c372ff848f660ea9c4372987440
Sa-Lin <devnull@localhost>
2014-09-02 16:23:53 -0500

    added default value 0.0001 to SLEPc 'tolerance for the residual' field.

--- src/Dlighthouse/lighthouse/forms/slepc_eprob.py
+++ src/Dlighthouse/lighthouse/forms/slepc_eprob.py
@@ -10,7 +10,7 @@ class SlepcGuidedForm(forms.Form):
 	size = forms.IntegerField(label='Enter approximate size of the matrix.',required=False)
 	numEigenvalues = forms.IntegerField(label='How many eigenvalues do you want?',required=False)
 	spectrum = forms.ChoiceField( choices=SPECTRUM_CHOICES, label='Select the desired portion of spectrum', widget=forms.RadioSelect(attrs={'onclick': "",'id':"spectrumid"}),required=True)
-	tolerance = forms.DecimalField(label='Enter the desired tolerance for the residual.',required=False)
+	tolerance = forms.DecimalField(label='Enter the desired tolerance for the residual.',required=False, initial=0.0001)
 	processors = forms.IntegerField(label='Enter the number of processors available to run the solver.', min_value=1,required=False)
 	precision = forms.ChoiceField( choices=PRECISION_CHOICES, label='Select your precision preference.', widget=forms.RadioSelect(attrs={'onclick': "",'id':"precisionid"}),required=False)
 
