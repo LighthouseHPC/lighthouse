015657bde0492fdc248f2fceed3d9558aa08bd82
Sa-Lin <sasa1688@gmail.com>
2014-10-22 14:40:51 -0500

    updated lapack_eigen url for advanced search result.

--- src/lighthouseProject/lighthouse/urls/lapack_eigen.py
+++ src/lighthouseProject/lighthouse/urls/lapack_eigen.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('lighthouse.views.lapack_eigen',
     url(r'^index/$', "index", name="lapack_eigen"),
     url(r'^guidedSearch/$', "guidedSearch", name="lapack_eigen_guidedSearch"),
     url(r'^advancedForm/$', "advancedForm", name="lapack_eigen_advancedForm"),
-    #url(r'^advancedSearch/$', "advancedSearch", name="lapack_eigen_advancedSearch"),
+    url(r'^advancedSearch/$', "advancedSearch", name="lapack_eigen_advancedSearch"),
 )
