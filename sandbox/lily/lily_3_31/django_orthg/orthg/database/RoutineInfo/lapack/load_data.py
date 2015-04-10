cdabee8d3bb8abf4201d7e687c39c1299070c61d
liyo6397 <liyo6397@colorado.edu>
2015-04-10 10:21:12 -0600

    more

--- sandbox/lily/lily_3_31/django_orthg/orthg/database/RoutineInfo/lapack/load_data.py
+++ sandbox/lily/lily_3_31/django_orthg/orthg/database/RoutineInfo/lapack/load_data.py
@@ -1,9 +1,9 @@
 import csv
 import sqlite3 as lite
 
-myDB = lite.connect('orthg.db') 
+myDB = lite.connect('/home/lily/lighthouse/sandbox/lily/lily_3_31/django_orthg/orthg.db') 
 
-cursor = myDB.cursor()
+cur = myDB.cursor()
 
 
 reader = csv.reader(open("routine_info.csv"))
@@ -20,11 +20,14 @@ else:
 	
 for file in reader:
 	fd = open("RoutineTxt/"+file[0], "r")
-	myDB.query("insert into lighthouse_lapack_routineinfo (id, routine, info) values (\'%d\', \'%s\', \'%s\')" % (i, file[0], myDB.escape_string(fd.read())))
-	i += 1
+        try: 
+       	    myDB.execute("insert or ignore into orthg_lapack_routineinfo(id, routine, info) values(?,?,?)", (i, file[0], fd.read()))
+        except lite.IntegrityError as err:
+               print(err)
+	i += 1     
 
 
 myDB.commit()
-cursor.close()
+cur.close()
 myDB.close()
 
