5a5137ccf57fc9d3a459c4b6d2539e1719d49c5b
Boyana Norris <brnorris03@gmail.com>
2015-06-03 14:15:00 -0700

    more timing

--- sandbox/petsc/timing/cleanup.sh
+++ sandbox/petsc/timing/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-find . -type f -name .\* -mmin +120 | xargs /bin/rm
-find . -name \*.log -mmin +120 -size -1500c | xargs /bin/rm
+me \*.log -mmin +120 -size -1500c
+find . -tnype f -name .\* -mmin +120 | xargs /bin/rm
+find . -type f -name \*.log -mm +120 -size -1500c | xargs /bin/rm
