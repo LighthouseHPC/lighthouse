e61b2feb61b085bb2fca3676dc9f5fb46a60b046
Boyana Norris <brnorris03@gmail.com>
2015-12-02 03:14:08 +0000

    done update

--- sandbox/petsc/new/solverscript-bgq.py
+++ sandbox/petsc/new/solverscript-bgq.py
@@ -7,6 +7,7 @@ from solvers import *
 
 # Directory contaning the *.petsc matrices:
 wdir='/gpfs/mira-fs0/projects/PEACEndStation/norris/UFloridaSparseMat/'
+wdir='/home/norris/UFloridaSparseMat/'
 # Directory for storing results:
 tdir=wdir+'timing/'
 cdir=os.getcwd() 
@@ -17,8 +18,10 @@ nprocs = 2048    # run with qsub -n 128 --proccount 2048 --mode c16 -t 60
 p = 16
 matrices = glob.glob(wdir+'petsc/*.petsc')
 donelist=[]
-if os.path.exists('DONE_TRILINOS'):
-  donelist=open('DONE_TRILINOS','r').readlines()
+#if os.path.exists('DONE_TRILINOS'):
+#  donelist=open('DONE_TRILINOS','r').readlines()
+if os.path.exists('DONE'):
+  donelist=open('DONE','r').readlines()
 else:
   print "Error: can't find done matrix file"
   exit(1)
@@ -38,8 +41,8 @@ random.shuffle(hashlist)
 for hash in hashlist:
   solver_optstr = solveropts[hash]
   if totalprocs > nprocs: break
-  for matname in donelist:
-    matname = matname.strip()
+  for mat in donelist:
+    matname = mat.strip('\n')
     matrixpath=wdir+'petsc/'+matname+'.petsc'
     if not os.path.exists(matrixpath):
       #print "No PETSc matrix:", matrixpath
@@ -48,18 +51,21 @@ for hash in hashlist:
       #print "PETSc matrix:", matrixpath
     if totalprocs > nprocs: break
     lockfile = tdir + '.%s.%s' % (matname, str(hash))
-    logfile = tdir + '%s.%s.log' % (matname, str(hash))
+    logfile = tdir + '/tmp/%s.%s.log' % (matname, str(hash))
     #print "Logfile:", logfile
     if os.path.exists(lockfile) or os.path.exists(logfile): continue
     opts = ['-f ',wdir+'petsc/'+matname+'.petsc', '-hash', hash, solver_optstr]
     cmd = os.path.join(cdir,'parallel-bgq')
     buf += 'runjob --np 1 -p ' + str(p) + ' --block $COBALT_PARTNAME --verbose=INFO : ' + cmd + ' ' + ' '.join(opts) + ' > ' + logfile  + ' \n'
+    totalprocs += p
     #print cmd + ' ' + ' '.join(opts)
 
 
-f = open('parallel%d.sh' %p,'w')
+scriptname = 'parallel%d.sh' %p
+f = open(scriptname,'w')
 f.write(buf)
 f.close()
+os.system("chmod +x %s" % scriptname)
 
 
  
