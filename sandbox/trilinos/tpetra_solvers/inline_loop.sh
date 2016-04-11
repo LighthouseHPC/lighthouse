bb3872ed1a116665587780e0a9b4db3430811d17
Pate Motter <patemotter@gmail.com>
2016-04-11 12:56:53 -0600

    Adds csv updating to the batch script

--- sandbox/trilinos/tpetra_solvers/inline_loop.sh
+++ sandbox/trilinos/tpetra_solvers/inline_loop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=UF.1000+nnz.np12
+#SBATCH --job-name=UF.1000+nnz.np1
 #SBATCH --qos=janus
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
+#SBATCH --reservation=janus-serial
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=1
 #SBATCH --output=/lustre/janus_scratch/pamo8800/All_Results/slurm-%j.out
 
 DATE=`date +%m.%d.%Y_%H:%M`
@@ -12,10 +13,21 @@ OUTDIR="/lustre/janus_scratch/pamo8800/All_Results/${SLURM_JOB_NAME}__${DATE}__$
 MATDIR=/lustre/janus_scratch/pamo8800/UF_Collection_Matrix-Market
 EXEDIR=~/project/lighthouse/sandbox/trilinos/tpetra_solvers
 
+INPUT=$1
+
 mkdir -p $OUTDIR
 
-while read -r f <&3
+OLDIFS=$IFS
+IFS=,
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read matrix solved <&3
 do
-    echo $f
-    mpirun -np 12 $EXEDIR/tpetra_solvers $MATDIR/$f -f $OUTDIR/$f.out
-done 3< $1
+    if [ $solved -eq 0 ]
+    then
+        echo solving $matrix
+        mpirun $EXEDIR/tpetra_solvers $MATDIR/$matrix -f $OUTDIR/$matrix.out && sed -i "s/${matrix}, 0/${matrix}, 1/g" "$1"
+    else
+        echo skipping $matrix
+    fi
+done 3< $INPUT
+IFS=$OLDIFS
