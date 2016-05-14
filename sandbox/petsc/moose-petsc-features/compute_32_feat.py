459cef86864bcf200623395d1557afd7440e70d9
kanika901 <kanika901@gmail.com>
2016-05-14 11:27:42 -0700

    updated csv writer script

--- sandbox/petsc/moose-petsc-features/compute_32_feat.py
+++ sandbox/petsc/moose-petsc-features/compute_32_feat.py
@@ -1,13 +1,13 @@
 import os,csv
+
 folder = "/home8/kanika/moose/mat_files/"
 output = '/home8/kanika/petsc2/src/ksp/ksp/examples/tutorials/moose_features/tutorials/script.sh'
-
 target = open(output,'w')
 target.write("clear \n")
-                        #write the header to the new decoupled file before writing the feature values
-target1 = open('/home8/kanika/petsc2/src/ksp/ksp/examples/tutorials/moose_features/tutorials/results.csv','w')
 
-target1.write("MinNonzerosPerRow" + "," + "RowVariance" +  "," + "ColumnVariance" + "," + 
+feat_file = open('/home8/kanika/petsc2/src/ksp/ksp/examples/tutorials/moose_features/tutorials/results.csv','w')
+#write the header to the new decoupled file before writing the feature values
+feat_file.write("MinNonzerosPerRow" + "," + "RowVariance" +  "," + "ColumnVariance" + "," + 
 "DiagonalVariance" + ","+ "Nonzeros " + ","+ "Dimension" + ","+ "FrobeniusNorm " + ","+ 
 "SymmetricFrobeniusNorm " + ","+ "AntiSymmetricFrobeniusNorm" + "," + "OneNorm" + "," + 
 "InfinityNorm" + ","+ " SymmetricInfinityNorm" + ","+ "AntiSymmetricInfinityNorm" + ","+
@@ -18,7 +18,7 @@ target1.write("MinNonzerosPerRow" + "," + "RowVariance" +  "," + "ColumnVariance
  "DiagonalSign " + ","+ "DiagonalNonZeros " + ","+ "lowerBandwidth " + ","+ "upperBandwidth " + 
  ","+ "MatrixSymmetric " + "," +"matrix"+ "\n")
 
-for file1 in os.listdir(folder):
-				target.write("./ex100 -f " + folder + file1 + " >> results.csv" + "\n") 
+for mat_file in os.listdir(folder):
+	target.write("./ex100 -f " + folder + mat_file + " >> results.csv" + "\n") 
 
 target.write("exit 0")
